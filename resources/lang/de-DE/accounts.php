1eb8ce0ea5c486eac064efdb7518ad4213f74974
cracker182 <cracker182@outlook.de>
2017-09-19 10:20:38 +0200

    german Translation
    Adds german translation for accounts.php

---
+++ resources/lang/de-DE/accounts.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'account_name'          => 'Konto Name',
+    'number'                => 'Nummer',
+    'opening_balance'       => 'Start Saldo',
+    'current_balance'       => 'Aktuelles Saldo',
+    'bank_name'             => 'Bank Name',
+    'bank_phone'            => 'Bank Telefon',
+    'bank_address'          => 'Bank Adresse',
+    'default_account'       => 'Standard Konto',
+    'all'                   => 'Alle Konten',
+];
