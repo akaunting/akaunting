3b0b38b3aac491fd4190a29883e556b46ff5f0ea
denisdulici <denisdulici@gmail.com>
2017-10-11 20:35:51 +0300

    missing model class #46

--- database/migrations/2017_10_11_000000_create_invoice_totals_table.php
+++ database/migrations/2017_10_11_000000_create_invoice_totals_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use App\Models\Model;
 use App\Models\Company\Company;
 use App\Models\Income\Invoice;
 use App\Models\Income\InvoiceItem;
