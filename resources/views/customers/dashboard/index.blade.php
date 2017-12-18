681aef0da03e9be6e7d13573d80add8931104556
cuneytsenturk <cneytsenturk@gmail.com>
2017-12-18 19:48:56 +0300

    customer panel dashboard add chart

--- resources/views/customers/dashboard/index.blade.php
+++ resources/views/customers/dashboard/index.blade.php
@@ -4,90 +4,63 @@
 
 @section('content')
     <div class="row">
+        <!---Income, Expense and Profit Line Chart-->
         <div class="col-md-12">
-            <!-- Invoices List-->
             <div class="box box-success">
-                <div class="box-header with-border">
-                    <h3 class="box-title">{{ trans_choice('general.invoices', 2) }}</h3>
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                            <i class="fa fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                <div class="box-body" id="chart">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="customer-content">
+                                <div class="pull-left">{{ trans('general.unpaid') }}</div>
+                                <div class="pull-right">{{ $progress['total'] }} / {{ $progress['unpaid'] }}</div>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="{{ (100 / $progress['total']) * $progress['unpaid'] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ (100 / $progress['total']) * $progress['unpaid'] }}%">
+                                        {{ (100 / $progress['total']) * $progress['unpaid'] }} %
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="customer-content">
+                                <div class="pull-left">{{ trans('general.paid') }}</div>
+                                <div class="pull-right">{{ $progress['total'] }} / {{ $progress['paid'] }}</div>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="{{ (100 / $progress['total']) * $progress['paid'] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ (100 / $progress['total']) * $progress['paid'] }}%">
+                                        {{ (100 / $progress['total']) * $progress['paid'] }}%
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="customer-content">
+                                <div class="pull-left">{{ trans('general.overdue') }}</div>
+                                <div class="pull-right">{{ $progress['total'] }} / {{ $progress['overdue'] }}</div>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="{{ (100 / $progress['total']) * $progress['overdue'] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ (100 / $progress['total']) * $progress['overdue'] }}%">
+                                        {{ (100 / $progress['total']) * $progress['overdue'] }}%
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="customer-content">
+                                <div class="pull-left">{{ trans('general.partially_paid') }}</div>
+                                <div class="pull-right">{{ $progress['total'] }} / {{ $progress['partially_paid'] }}</div>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-light-blue" role="progressbar" aria-valuenow="{{ (100 / $progress['total']) * $progress['partially_paid'] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ (100 / $progress['total']) * $progress['partially_paid'] }}%">
+                                        {{ (100 / $progress['total']) * $progress['partially_paid'] }}%
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="box-body">
-                    @if ($invoices->count())
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th class="col-md-2">{{ trans('invoices.invoice_number') }}</th>
-                                <th class="col-md-2 text-right">{{ trans('general.amount') }}</th>
-                                <th class="col-md-3 text-right">{{ trans('invoices.invoice_date') }}</th>
-                                <th class="col-md-3 text-right">{{ trans('invoices.due_date') }}</th>
-                                <th class="col-md-2 text-center">{{ trans_choice('general.statuses', 1) }}</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($invoices as $item)
-                            <tr>
-                                <td><a href="{{ url('customers/invoices/' . $item->id) }}">{{ $item->invoice_number }}</a></td>
-                                <td class="text-right">@money($item->amount, $item->currency_code, true)</td>
-                                <td class="text-right">{{ Date::parse($item->invoiced_at)->format($date_format) }}</td>
-                                <td class="text-right">{{ Date::parse($item->due_at)->format($date_format) }}</td>
-                                <td class="text-center"><span class="label {{ $item->status->label }}">{{ $item->status->name }}</span></td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    @else
-                        <h5 class="text-center">{{ trans('general.no_records') }}</h5>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <!-- Revenues List-->
-            <div class="box box-success">
-                <div class="box-header with-border">
-                    <h3 class="box-title">{{ trans_choice('general.payments', 2) }}</h3>
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                            <i class="fa fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-                    @if ($customer->revenues->count())
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th>{{ trans('general.date') }}</th>
-                                <th>{{ trans('general.amount') }}</th>
-                                <th>{{ trans_choice('general.categories', 1) }}</th>
-                                <th>{{ trans_choice('general.accounts', 1) }}</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($customer->revenues as $item)
-                            <tr>
-                                <td><a href="{{ url('customers/payments/' . $item->id . '') }}">{{ Date::parse($item->paid_at)->format($date_format) }}</a></td>
-                                <td>@money($item->amount, $item->currency_code, true)</td>
-                                <td>{{ $item->category->name }}</td>
-                                <td>{{ $item->account->name }}</td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    @else
-                        <h5 class="text-center">{{ trans('general.no_records') }}</h5>
-                    @endif
+                    <hr>
+                    {!! $chart->render() !!}
                 </div>
             </div>
         </div>
     </div>
 @endsection
+@push('js')
+{!! Charts::assets() !!}
+@endpush
