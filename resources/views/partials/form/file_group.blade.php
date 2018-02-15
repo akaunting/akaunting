5f7aeeba439d1bdfe012222382cbb97a14227a63
Mohamed Abo Bdwey <m.bdwey@gmail.com>
2018-02-15 21:14:18 +0200

    File only accepts name and options attributes

--- resources/views/partials/form/file_group.blade.php
+++ resources/views/partials/form/file_group.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group {{ $col }} {{ isset($attributes['required']) ? 'required' : '' }} {{ $errors->has($name) ? 'has-error' : '' }}" style="min-height: 59px">
     {!! Form::label($name, $text, ['class' => 'control-label']) !!}
-    {!! Form::file($name, $value, array_merge(['class' => 'form-control'], $attributes)) !!}
+    {!! Form::file($name, array_merge(['class' => 'form-control'], $attributes)) !!}
     {!! $errors->first($name, '<p class="help-block">:message</p>') !!}
 </div>
