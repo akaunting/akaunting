62560782c56f8ac8ef9516318fbb7688ba5dbd95
denisdulici <denisdulici@gmail.com>
2018-04-23 10:06:55 +0300

    el-GR translation

---
+++ resources/lang/el-GR/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'total_incomes'             => 'Συνολικά έσοδα',
+    'receivables'               => 'Εισπρακτέα',
+    'open_invoices'             => 'Εκρεμή τιμολόγια',
+    'overdue_invoices'          => 'Ανεξόφλητα τιμολόγια',
+    'total_expenses'            => 'Συνολικές δαπάνες',
+    'payables'                  => 'Πληρωτέα',
+    'open_bills'                => 'Ανοιχτοί λογαριασμοί',
+    'overdue_bills'             => 'Ανεξόφλητοι λογαριασμοί',
+    'total_profit'              => 'Συνολικό κέρδος',
+    'open_profit'               => 'Ανοιχτό κέρδος',
+    'overdue_profit'            => 'Εκπρόθεσμο κέρδος',
+    'cash_flow'                 => 'Ταμειακή ροή',
+    'no_profit_loss'            => 'Καμία απώλεια κέρδους',
+    'incomes_by_category'       => 'Έσοδα κατά κατηγορία',
+    'expenses_by_category'      => 'Έξοδα ανά κατηγορία',
+    'account_balance'           => 'Υπόλοιπο λογαριασμού',
+    'latest_incomes'            => 'Τελευταία έσοδα',
+    'latest_expenses'           => 'Τελευταία έξοδα',
+
+];
---
+++ resources/lang/el-GR/reports.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'this_year'             => 'Φέτος',
+    'previous_year'         => 'Προηγούμενο έτος',
+    'this_quarter'          => 'Αυτό το τρίμηνο',
+    'previous_quarter'      => 'Προηγούμενο τρίμηνο',
+    'last_12_months'        => 'Τελευταίοι 12 μήνες',
+
+    'summary' => [
+        'income'            => 'Σύνοψη εσόδων',
+        'expense'           => 'Σύνοψη εξόδων',
+        'income_expense'    => 'Έσοδα με έξοδα',
+    ],
+
+];
