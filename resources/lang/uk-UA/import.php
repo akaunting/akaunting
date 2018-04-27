715587d36a68a9bd7726c9771b67db67ca5459bc
denisdulici <denisdulici@gmail.com>
2018-04-27 17:48:21 +0300

    uk-UA translation

---
+++ resources/lang/uk-UA/import.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'import'                => 'Імпорт',
+    'title'                 => 'Імпорт: тип',
+    'message'               => 'Допускається типи файлів: CSV, XLS. Будь-ласка<a target="_blank" href=":link"><strong>завантажте</strong></a> зразок файлу.',
+
+];
