b5b83587ac824a49fde82f8439c47e6f5c4087f2
denisdulici <denisdulici@gmail.com>
2018-05-04 14:59:11 +0300

    fixed #326

--- app/Traits/Recurring.php
+++ app/Traits/Recurring.php
@@ -2,8 +2,6 @@
 
 namespace App\Traits;
 
-use DateTime;
-use DateTimeZone;
 use Recurr\Rule;
 use Recurr\Transformer\ArrayTransformer;
 use Recurr\Transformer\ArrayTransformerConfig;
@@ -131,7 +129,7 @@ trait Recurring
 
     public function getRuleStartDate()
     {
-        return new DateTime($this->started_at, new DateTimeZone($this->getRuleTimeZone()));
+        return new \DateTime($this->started_at, new \DateTimeZone($this->getRuleTimeZone()));
     }
 
     public function getRuleTimeZone()
