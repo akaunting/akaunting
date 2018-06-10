3a0a3490909faa201a96c6c04a5a787e8e2d645b
denisdulici <denisdulici@gmail.com>
2018-06-10 10:39:45 +0300

    small change

--- app/Models/Company/Company.php
+++ app/Models/Company/Company.php
@@ -2,9 +2,7 @@
 
 namespace App\Models\Company;
 
-use App\Models\Common\Company as C;
-
 /**
  * @deprecated since 1.2.7 version. use Common\Company instead.
  */
-class Company extends C {}
+class Company extends \App\Models\Common\Company {}
--- app/Models/Item/Item.php
+++ app/Models/Item/Item.php
@@ -2,9 +2,7 @@
 
 namespace App\Models\Item;
 
-use App\Models\Common\Item as I;
-
 /**
  * @deprecated since 1.2.7 version. use Common\Item instead.
  */
-class Item extends I {}
+class Item extends \App\Models\Common\Item {}
