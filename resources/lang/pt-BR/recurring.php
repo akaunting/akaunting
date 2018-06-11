e7cca4ace7a85c37640d218101502ca37361a187
Jonas Diel <jonasdiel@gmail.com>
2018-06-11 15:44:27 -0300

    Translating recurring.php to pt-BR

--- resources/lang/pt-BR/recurring.php
+++ resources/lang/pt-BR/recurring.php
@@ -2,19 +2,19 @@
 
 return [
 
-    'recurring'             => 'Recurring',
-    'every'                 => 'Every',
-    'period'                => 'Period',
-    'times'                 => 'Times',
-    'daily'                 => 'Daily',
-    'weekly'                => 'Weekly',
-    'monthly'               => 'Monthly',
-    'yearly'                => 'Yearly',
-    'custom'                => 'Custom',
-    'days'                  => 'Day(s)',
-    'weeks'                 => 'Week(s)',
-    'months'                => 'Month(s)',
-    'years'                 => 'Year(s)',
-    'message'               => 'This is a recurring :type and the next :type will be automatically generated at :date',
+    'recurring'             => 'Recorrente',
+    'every'                 => 'Sempre',
+    'period'                => 'Período',
+    'times'                 => 'Vezes',
+    'daily'                 => 'Diariamente',
+    'weekly'                => 'Semanalmente',
+    'monthly'               => 'Mensalmente',
+    'yearly'                => 'Anualmente',
+    'custom'                => 'Personalizado',
+    'days'                  => 'Dia(s)',
+    'weeks'                 => 'Semana(s)',
+    'months'                => 'Mês(es)',
+    'years'                 => 'Ano(s)',
+    'message'               => 'Este é um :type recorrente e o próximo :type será gerado automaticamente em :date',
 
 ];
