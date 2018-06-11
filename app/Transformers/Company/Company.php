0df65042d05f6b17ffafb98902d769380e54e6e2
denisdulici <denisdulici@gmail.com>
2018-06-11 13:39:41 +0300

    added transformers for BC

---
+++ app/Transformers/Company/Company.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Transformers\Company;
+
+/**
+ * @deprecated since 1.2.7 version. use Common\Company instead.
+ */
+class Company extends \App\Transformers\Common\Company {}
---
+++ app/Transformers/Item/Item.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Transformers\Item;
+
+/**
+ * @deprecated since 1.2.7 version. use Common\Item instead.
+ */
+class Item extends \App\Transformers\Common\Item {}
