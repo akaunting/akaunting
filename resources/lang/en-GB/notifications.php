2a1252e6fcee320578c85a87eb4b0c0b8a444b2a
cuneytsenturk <cneytsenturk@gmail.com>
2018-07-18 17:54:50 +0300

    close #422 Added: Notification language text

---
+++ resources/lang/en-GB/notifications.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'whoops'              => 'Whoops!',
+    'hello'               => 'Hello!',
+    'salutation'          => 'Regards,<br> :company_name',
+    'subcopy'             => 'If you’re having trouble clicking the ":text" button, copy and paste the URL below
+into your web browser: [:url](:url)',
+
+];
