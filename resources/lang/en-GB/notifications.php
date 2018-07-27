39d0b6e185214c69a0bb9598de9aeb78ff789166
denisdulici <denisdulici@gmail.com>
2018-07-27 12:07:52 +0300

    string update

--- resources/lang/en-GB/notifications.php
+++ resources/lang/en-GB/notifications.php
@@ -5,7 +5,6 @@ return [
     'whoops'              => 'Whoops!',
     'hello'               => 'Hello!',
     'salutation'          => 'Regards,<br> :company_name',
-    'subcopy'             => 'If you’re having trouble clicking the ":text" button, copy and paste the URL below
-into your web browser: [:url](:url)',
+    'subcopy'             => 'If you’re having trouble clicking the ":text" button, copy and paste the URL below into your web browser: [:url](:url)',
 
 ];
