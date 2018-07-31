fe00f48bf064b43a38d72abf91c57d142cecf737
cuneytsenturk <cneytsenturk@gmail.com>
2018-07-31 18:00:36 +0300

    close #436 Fixed: if permission and role code is the same, it broken.

--- app/Http/Requests/Auth/Permission.php
+++ app/Http/Requests/Auth/Permission.php
@@ -24,7 +24,7 @@ class Permission extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
+            'name' => 'required|string|unique:permissions,name',
             'display_name' => 'required|string',
         ];
     }
--- app/Http/Requests/Auth/Role.php
+++ app/Http/Requests/Auth/Role.php
@@ -24,7 +24,7 @@ class Role extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
+            'name' => 'required|string|unique:rules,name',
             'display_name' => 'required|string',
             'permissions' => 'required'
         ];
