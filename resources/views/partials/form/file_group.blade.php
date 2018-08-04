7b206fb2aa2ddaa674a851286ef572e83c77b725
denisdulici <denisdulici@gmail.com>
2018-08-04 19:07:32 +0300

    stacks for each form input #443

--- resources/views/partials/form/file_group.blade.php
+++ resources/views/partials/form/file_group.blade.php
@@ -1,5 +1,9 @@
+@stack($name . '_input_start')
+
 <div class="form-group {{ $col }} {{ isset($attributes['required']) ? 'required' : '' }} {{ $errors->has($name) ? 'has-error' : '' }}" style="min-height: 59px">
     {!! Form::label($name, $text, ['class' => 'control-label']) !!}
     {!! Form::file($name, array_merge(['class' => 'form-control'], $attributes)) !!}
     {!! $errors->first($name, '<p class="help-block">:message</p>') !!}
 </div>
+
+@stack($name . '_input_end')
