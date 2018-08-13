efe69843ee106bb6e24345dabf4e0036349b264d
Shreyansh Panchal <sunpan835@gmail.com>
2018-08-13 17:25:22 +0530

    Popover fix
    Fixed: Calling popover with html variable under quotes.

--- resources/views/expenses/bills/create.blade.php
+++ resources/views/expenses/bills/create.blade.php
@@ -273,7 +273,7 @@
             });
 
             $('a[rel=popover]').popover({
-                html: 'true',
+                html: true,
                 placement: 'bottom',
                 title: '{{ trans('bills.discount') }}',
                 content: function () {
