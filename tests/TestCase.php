03930275714d5014dd8c9366233a8e46ef809e2f
denisdulici <denisdulici@gmail.com>
2018-10-01 10:36:01 +0300

    changed tests for ci

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Artisan;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('db:seed', ['--class' => '\Database\Seeds\TestCompany', '--force' => true]);
+        Artisan::call('company:seed',['company' => 1]);
+    }
 }
