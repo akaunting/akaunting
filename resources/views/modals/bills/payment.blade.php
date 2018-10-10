da6ea45ecb3a0764c35e7daee997d19ba7bdd391
denisdulici <denisdulici@gmail.com>
2018-10-10 18:07:41 +0300

    fixed payment modal title

--- resources/views/modals/bills/payment.blade.php
+++ resources/views/modals/bills/payment.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">{{ trans('general.title.new', ['type' => trans_choice('general.categories', 1)]) }}</h4>
+                <h4 class="modal-title">{{ trans('general.title.new', ['type' => trans_choice('general.payments', 1)]) }}</h4>
             </div>
             <div class="modal-body">
                 <div class="modal-message"></div>
