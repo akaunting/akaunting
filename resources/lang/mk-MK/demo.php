7d3fca10409f3e375eebdd438c72922073bde5db
denisdulici <denisdulici@gmail.com>
2018-10-10 17:40:26 +0300

    mk-MK translation

---
+++ resources/lang/mk-MK/demo.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'accounts_cash'         => 'Готовина',
+    'categories_deposit'    => 'Депозит',
+    'categories_sales'      => 'Продажби',
+    'currencies_usd'        => 'Долар',
+    'currencies_eur'        => 'Евро',
+    'currencies_gbp'        => 'Фунти',
+    'currencies_try'        => 'Турски лири',
+    'taxes_exempt'          => 'Ослободен од данок',
+    'taxes_normal'          => 'Нормална стапка на данок',
+    'taxes_sales'           => 'Данок на продажба',
+
+];
---
+++ resources/lang/mk-MK/import.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'import'                => 'Внеси',
+    'title'                 => 'Внеси :type',
+    'message'               => 'Дозволени типови на фајлови: XLS, XLSX. Ве молиме, <a target="_blank" href=":link"><strong>симнете го</strong></a> фајлот.
+ 
+',
+
+];
