f8566a79c9428d34849dfa7fbe03e71907dca26d
cuneytsenturk <cneytsenturk@gmail.com>
2018-11-12 17:09:56 +0300

    close #622 Fixed: If the app use pagination, It not change title.

--- resources/views/partials/admin/pagination.blade.php
+++ resources/views/partials/admin/pagination.blade.php
@@ -2,7 +2,7 @@
 
 @if ($items->firstItem())
     <div class="pull-left" style="margin-top: 7px;">
-        <small>{{ trans('pagination.showing', ['first' => $items->firstItem(), 'last' => $items->lastItem(), 'total' => $items->total(), 'type' => strtolower(trans_choice('general.' . $type, 2))]) }}</small>
+        <small>{{ trans('pagination.showing', ['first' => $items->firstItem(), 'last' => $items->lastItem(), 'total' => $items->total(), 'type' => strtolower((isset($title)) ? $title : trans_choice('general.' . $type, 2))]) }}</small>
     </div>
     <div class="pull-right">
         {!! $items->appends(request()->except('page'))->links() !!}
