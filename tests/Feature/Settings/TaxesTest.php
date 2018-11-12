597a0c53b525ca961deafd68e79ceaf20f09620e
Batuhan Baş <hawkfreedev@gmail.com>
2018-11-12 11:05:27 +0300

    Request value is changed

--- tests/Feature/Settings/TaxesTest.php
+++ tests/Feature/Settings/TaxesTest.php
@@ -67,7 +67,7 @@ class TaxesTest extends FeatureTestCase
             'company_id' => $this->company->id,
             'name' => $this->faker->text(15),
             'rate' => $this->faker->randomFloat(2, 10, 20),
-            'type' => 'Normal',
+            'type' => $this->faker->randomElement(['normal', 'inclusive', 'compound']),
             'enabled' => $this->faker->boolean ? 1 : 0
         ];
     }
