4020d29f814014815a9158c97850b7cbc29f3d36
Batuhan Baş <hawkfreedev@gmail.com>
2018-12-22 20:19:42 +0300

    tax add button icon is changed

--- resources/views/incomes/invoices/edit.blade.php
+++ resources/views/incomes/invoices/edit.blade.php
@@ -314,7 +314,7 @@
                             },
                             language: {
                                 noResults: function () {
-                                    return '<span id="tax-add-new"><i class="fa fa-plus"></i> {{ trans('general.title.new', ['type' => trans_choice('general.tax_rates', 1)]) }}</span>';
+                                    return '<span id="tax-add-new"><i class="fa fa-plus-circle"></i> {{ trans('general.title.new', ['type' => trans_choice('general.tax_rates', 1)]) }}</span>';
                                 }
                             }
                         });
