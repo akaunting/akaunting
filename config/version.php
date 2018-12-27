2521c2f3521dbd363e20c4ba7eeefe7cc18f5f1e
cuneytsenturk <cneytsenturk@gmail.com>
2018-12-27 15:49:00 +0300

    version update 1.3.6 to 1.3.7

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '3',
 
-    'patch'     =>  '6',
+    'patch'     =>  '7',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '26-December-2018',
+    'date'      =>  '27-December-2018',
 
-    'time'      =>  '17:30',
+    'time'      =>  '16:00',
 
     'zone'      =>  'GMT +3',
 
