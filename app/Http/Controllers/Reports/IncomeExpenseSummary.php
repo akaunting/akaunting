ed12662bfb900254c63bf933dd567b4ad58dd495
Batuhan Baş <hawkfreedev@gmail.com>
2018-12-27 11:41:52 +0300

    forgotten code line

--- app/Http/Controllers/Reports/IncomeExpenseSummary.php
+++ app/Http/Controllers/Reports/IncomeExpenseSummary.php
@@ -32,7 +32,6 @@ class IncomeExpenseSummary extends Controller
         $year = request('year', Date::now()->year);
 
         // check and assign year start
-       //if (($financial_start = Date::parse(setting('general.financial_start')))->month != 1) {
         if (($financial_start = Date::parse(setting('general.financial_start'))->month) != 1) {
                 // check if a specific year is requested
                 if (!is_null(request('year'))) {
