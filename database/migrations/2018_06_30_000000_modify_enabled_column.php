e2038f4e4605a05e6f7bca0abb9ccbc800c1c4a4
Shahal Tharique <shahalpk@gmail.com>
2019-03-24 00:58:51 +0530

    Fixed
    1. Incompatible types issue during Postgresql migration

--- database/migrations/2018_06_30_000000_modify_enabled_column.php
+++ database/migrations/2018_06_30_000000_modify_enabled_column.php
@@ -12,6 +12,14 @@ class ModifyEnabledColumn extends Migration
      */
     public function up()
     {
+        
+        if ( DB::connection()->getPDO()->getAttribute(PDO::ATTR_DRIVER_NAME) == 'pgsql' ){ 
+            $currencyTableName = DB::connection()->getTablePrefix().(with(new App\Models\Setting\Currency)->getTable());
+            DB::statement("ALTER TABLE $currencyTableName ALTER COLUMN enabled DROP DEFAULT");
+            DB::statement("ALTER TABLE $currencyTableName ALTER enabled TYPE bool USING CASE WHEN enabled=1 THEN TRUE ELSE FALSE END");
+            DB::statement("ALTER TABLE $currencyTableName ALTER COLUMN enabled SET DEFAULT FALSE");
+            return;
+        }
         Schema::table('accounts', function (Blueprint $table) {
             $table->boolean('enabled')->default(1)->change();
         });
