5455835ac882315776f0819cd4a5ef9e936e0b1a
denisdulici <denisdulici@gmail.com>
2019-11-17 17:51:54 +0300

    formatting

--- resources/views/settings/invoice/edit.blade.php
+++ resources/views/settings/invoice/edit.blade.php
@@ -24,12 +24,6 @@
 
                 {{ Form::textGroup('number_next', trans('settings.invoice.next'), 'chevron-right', []) }}
 
-                {{ Form::invoice_text('item_name', trans('settings.invoice.item_name'), 'font', $item_names, null, [], 'item_name_input', null) }}
-
-                {{ Form::invoice_text('price_name', trans('settings.invoice.price_name'), 'font', $price_names, null, [], 'price_name_input', null) }}
-
-                {{ Form::invoice_text('quantity_name', trans('settings.invoice.quantity_name'), 'font', $quantity_names, null, [], 'quantity_name_input', null) }}
-
                 {{ Form::selectGroup('payment_terms', trans('settings.invoice.payment_terms'), 'calendar', $payment_terms, $setting['payment_terms'], []) }}
 
                 {{ Form::textGroup('title', trans('settings.invoice.title'), 'font', []) }}
@@ -39,6 +33,12 @@
                 {{ Form::textareaGroup('notes', trans_choice('general.notes', 2)) }}
 
                 {{ Form::textareaGroup('footer', trans('general.footer')) }}
+
+                {{ Form::invoice_text('item_name', trans('settings.invoice.item_name'), 'font', $item_names, null, [], 'item_name_input', null) }}
+
+                {{ Form::invoice_text('price_name', trans('settings.invoice.price_name'), 'font', $price_names, null, [], 'price_name_input', null) }}
+
+                {{ Form::invoice_text('quantity_name', trans('settings.invoice.quantity_name'), 'font', $quantity_names, null, [], 'quantity_name_input', null) }}
             </div>
         </div>
 
