fc8518e35e2c13b6f392ae0ef31f64a8dc2b0c0b
denisdulici <denisdulici@gmail.com>
2019-12-04 21:21:15 +0300

    php 7.4

--- app/Models/Income/InvoiceTotal.php
+++ app/Models/Income/InvoiceTotal.php
@@ -76,9 +76,9 @@ class InvoiceTotal extends Model
             $title .= ' (';
 
             if (setting('localisation.percent_position', 'after') == 'after') {
-                $title .= $this->code == 'discount' ? false : $tax->type == 'fixed' ? $percent : $percent . '%';
+                $title .= ($this->code == 'discount') ? false : (($tax->type == 'fixed') ? $percent : $percent . '%');
             } else {
-                $title .= $this->code == 'discount' ? false : $tax->type == 'fixed' ? $percent : '%' . $percent;
+                $title .= ($this->code == 'discount') ? false : (($tax->type == 'fixed') ? $percent : '%' . $percent);
             }
 
             $title .= ')';
