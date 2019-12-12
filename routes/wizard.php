68a9f1add74dc75c5f051ac5b38e9dd2c7994bce
Sevan Nerse <nerse.sevan@gmail.com>
2019-12-12 11:43:13 +0300

    Fixing format changes

--- routes/wizard.php
+++ routes/wizard.php
@@ -1,19 +1,20 @@
 <?php
-
-Route::group(['as' => 'wizard.'], function () {
+Route::group([
+    'as' => 'wizard.'
+], function () {
     Route::get('companies', 'Wizard\Companies@edit')->name('companies.edit');
     Route::patch('companies', 'Wizard\Companies@update')->name('companies.update');
-    
+
     Route::get('currencies', 'Wizard\Currencies@index')->name('currencies.index');
     Route::delete('currencies/{currency}', 'Wizard\Currencies@destroy')->name('currencies.delete');
     Route::post('currencies', 'Wizard\Currencies@store')->name('currencies.store');
     Route::patch('currencies/{currency}', 'Wizard\Currencies@update')->name('currencies.update');
-    
+
     Route::get('taxes', 'Wizard\Taxes@index')->name('taxes.index');
     Route::get('taxes/{tax}/delete', 'Wizard\Taxes@destroy')->name('taxes.delete');
     Route::post('taxes', 'Wizard\Taxes@store')->name('taxes.store');
     Route::patch('taxes/{tax}', 'Wizard\Taxes@update')->name('taxes.update');
-    
+
     Route::get('finish', 'Wizard\Finish@index')->name('finish.index');
 });
     
