c33a72d92ba235fe98f5531ab5466a6a73549649
Cüneyt Şentürk <cneytsenturk@gmail.com>
2019-12-16 17:22:34 +0300

    close #1003 Fixed: Pagination limit change feature not working

--- resources/views/partials/admin/pagination.blade.php
+++ resources/views/partials/admin/pagination.blade.php
@@ -4,7 +4,7 @@
             <span class="table-text hidden-lg">
                 {{ trans('general.show') }}
             </span>
-            {!! Form::select('limit', $limits, request('limit', setting('default.list_limit', '25')), ['class' => 'form-control form-control-sm table-header-search hidden-md', 'onchange' => 'this.form.submit()']) !!}
+            {!! Form::select('limit', $limits, request('limit', setting('default.list_limit', '25')), ['class' => 'form-control form-control-sm table-header-search hidden-md', '@change' => 'onChangePaginationLimit($event)']) !!}
             <span class="table-text hidden-lg">
                 {{ trans('pagination.page') }}
                 {{ trans('pagination.showing', ['first' => $items->firstItem(), 'last' => $items->lastItem(), 'total' => $items->total(), 'type' => strtolower((isset($title)) ? $title : trans_choice('general.' . $type, 2))]) }}
