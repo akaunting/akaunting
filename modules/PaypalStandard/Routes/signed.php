bbd87e6623cb6fce90ee514242ab9d2439264d3f
Cüneyt Şentürk <cneytsenturk@gmail.com>
2019-12-21 12:45:45 +0300

    Paypal Standard signed issue solved.

--- modules/PaypalStandard/Routes/signed.php
+++ modules/PaypalStandard/Routes/signed.php
@@ -5,5 +5,5 @@ Route::group([
     'middleware' => 'signed',
     'namespace' => 'Modules\PaypalStandard\Http\Controllers'
 ], function () {
-    Route::post('invoices/{invoice}/paypal-standard', 'Payment@show')->name('signed.invoices.paypal-standard.show');
+    Route::get('invoices/{invoice}/paypal-standard', 'Payment@show')->name('signed.invoices.paypal-standard.show');
 });
