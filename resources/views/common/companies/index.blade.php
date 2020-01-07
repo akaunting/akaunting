a5c694b2b7b0149532d8831e6c40e1d917223231
denisdulici <denisdulici@gmail.com>
2020-01-07 14:20:50 +0300

    missing company name

--- resources/views/common/companies/index.blade.php
+++ resources/views/common/companies/index.blade.php
@@ -74,7 +74,7 @@
                                         <a class="dropdown-item" href="{{ url('common/companies/' . $item->id . '/edit') }}">{{ trans('general.edit') }}</a>
                                         @permission('delete-common-companies')
                                             <div class="dropdown-divider"></div>
-                                            {!! Form::deleteLink($item, 'common/companies', '', 'company_name') !!}
+                                            {!! Form::deleteLink($item, 'common/companies') !!}
                                         @endpermission
                                     </div>
                                 </div>
