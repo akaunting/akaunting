84a37fd5242b5962271506b40fe1994139dd7437
batuhanbas <b1atuhanb1as@gmail.com>
2020-01-17 16:26:08 +0300

    reports print view updated

--- resources/views/partials/reports/table/rows.blade.php
+++ resources/views/partials/reports/table/rows.blade.php
@@ -1,9 +1,9 @@
 @php $row_total = 0; @endphp
 <tr>
-    <td class="report-column">{{ $class->getTableRowList()[$id] }}</td>
+    <td class="report-column rp-border-top-1">{{ $class->getTableRowList()[$id] }}</td>
     @foreach($items as $item)
         @php $row_total += $item; @endphp
-        <td class="report-column text-right px-0">@money($item, setting('default.currency'), true)</td>
+        <td class="report-column text-right px-0 rp-border-top-1">@money($item, setting('default.currency'), true)</td>
     @endforeach
-    <td class="report-column text-right">@money($row_total, setting('default.currency'), true)</td>
+    <td class="report-column text-right rp-border-top-1">@money($row_total, setting('default.currency'), true)</td>
 </tr>
--- resources/views/reports/profit_loss/content/header.blade.php
+++ resources/views/reports/profit_loss/content/header.blade.php
@@ -2,11 +2,11 @@
     <table class="table align-items-center">
         <thead class="border-top-style">
             <tr>
-                <th class="report-column text-right"></th>
+                <th class="report-column text-right rp-border-bottom-1"></th>
                 @foreach($class->dates as $date)
-                    <th class="report-column text-right px-0">{{ $date }}</th>
+                    <th class="report-column text-right px-0 rp-border-bottom-1">{{ $date }}</th>
                 @endforeach
-                <th class="report-column text-right">
+                <th class="report-column text-right rp-border-bottom-1">
                     {{ trans_choice('general.totals', 1) }}
                 </th>
             </tr>
