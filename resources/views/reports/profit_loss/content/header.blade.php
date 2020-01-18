1337664841fd0666f616755b7a2f71498b9b70db
batuhanbas <b1atuhanb1as@gmail.com>
2020-01-18 18:45:40 +0300

    report print updated

--- resources/views/reports/profit_loss/content/header.blade.php
+++ resources/views/reports/profit_loss/content/header.blade.php
@@ -1,12 +1,12 @@
 <div class="table-responsive overflow-auto">
-    <table class="table align-items-center">
+    <table class="table align-items-center rp-border-collapse">
         <thead class="border-top-style">
-            <tr>
-                <th class="report-column text-right rp-border-bottom-1"></th>
+            <tr class="rp-border-bottom-1">
+                <th class="report-column text-right"></th>
                 @foreach($class->dates as $date)
-                    <th class="report-column text-right px-0 rp-border-bottom-1">{{ $date }}</th>
+                    <th class="report-column text-right px-0">{{ $date }}</th>
                 @endforeach
-                <th class="report-column text-right rp-border-bottom-1">
+                <th class="report-column text-right">
                     {{ trans_choice('general.totals', 1) }}
                 </th>
             </tr>
--- resources/views/reports/profit_loss/table/footer.blade.php
+++ resources/views/reports/profit_loss/table/footer.blade.php
@@ -1,11 +1,11 @@
 <tfoot>
-    <tr>
-        <th class="report-column rp-border-top-1 text-left">{{ trans_choice('general.totals', 1) }}</th>
+    <tr class="rp-border-top-1">
+        <th class="report-column text-left">{{ trans_choice('general.totals', 1) }}</th>
         @php $total_total = 0; @endphp
         @foreach($class->totals[$table] as $date => $total)
             @php $total_total += $total; @endphp
-            <th class="report-column text-right px-0 rp-border-top-1">@money($total, setting('default.currency'), true)</th>
+            <th class="report-column text-right px-0">@money($total, setting('default.currency'), true)</th>
         @endforeach
-        <th class="report-column text-right rp-border-top-1">@money($total_total, setting('default.currency'), true)</th>
+        <th class="report-column text-right">@money($total_total, setting('default.currency'), true)</th>
     </tr>
 </tfoot>
