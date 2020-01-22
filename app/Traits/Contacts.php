46f8c44fff14acd3bfd0531010b0b2e78eb5d864
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-01-22 21:24:00 +0300

    Set contact types (customer/vendor)

--- app/Traits/Contacts.php
+++ app/Traits/Contacts.php
@@ -17,4 +17,29 @@ trait Contacts
 
         return ($return == 'array') ? explode(',', $types) : $types;
     }
+
+    public function setCustomerType($new_type)
+    {
+        $this->setType($new_type, 'customer');
+    }
+
+    public function setVendorType($new_type)
+    {
+        $this->setType($new_type, 'vendor');
+    }
+
+    public function setType($new_type, $index)
+    {
+        $types = explode(',', setting('contact.type.' . $index, $index));
+
+        if (in_array($new_type, $types)) {
+            return;
+        }
+
+        $types[] = $new_type;
+
+        setting([
+            'contact.type.' . $index => implode(',', $types),
+        ])->save();
+    }
 }
