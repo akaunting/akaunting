950df870a557337d13e0a5c67a62005205ed92e6
denisdulici <denisdulici@gmail.com>
2020-01-28 09:06:22 +0300

    styling

--- resources/views/partials/reports/table.blade.php
+++ resources/views/partials/reports/table.blade.php
@@ -1,19 +1,19 @@
 <div class="table-responsive overflow-auto mt-4">
     <table class="table align-items-center rp-border-collapse">
         @include($class->views['table.header'])
-            <tbody>
-                @if (!empty($class->row_values[$table]))
-                    @foreach($class->row_values[$table] as $id => $rows)
-                        @include($class->views['table.rows'])
-                    @endforeach
-                @else
-                    <tr>
-                        <td colspan="{{ count($class->dates) + 2 }}">
-                            <h5 class="text-center pl-0">{{ trans('general.no_records') }}</h5>
-                        </td>
-                    </tr>
-                @endif
-            </tbody>
+        <tbody>
+            @if (!empty($class->row_values[$table]))
+                @foreach($class->row_values[$table] as $id => $rows)
+                    @include($class->views['table.rows'])
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="{{ count($class->dates) + 2 }}">
+                        <h5 class="text-center pl-0">{{ trans('general.no_records') }}</h5>
+                    </td>
+                </tr>
+            @endif
+        </tbody>
         @include($class->views['table.footer'])
     </table>
 </div>
