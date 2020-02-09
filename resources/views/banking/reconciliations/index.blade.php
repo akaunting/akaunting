92f7825c718b5ea686028fb49e6860fafe0debe1
batuhanbas <b1atuhanb1as@gmail.com>
2020-02-09 13:14:24 +0300

    blank pages updated

--- resources/views/banking/reconciliations/index.blade.php
+++ resources/views/banking/reconciliations/index.blade.php
@@ -9,80 +9,84 @@
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-header border-bottom-0" :class="[{'bg-gradient-primary': bulk_action.show}]">
-            {!! Form::open([
-                'method' => 'GET',
-                'route' => 'reconciliations.index',
-                'role' => 'form',
-                'class' => 'mb-0'
-            ]) !!}
-                <div class="align-items-center" v-if="!bulk_action.show">
-                    <akaunting-search
-                        :placeholder="'{{ trans('general.search_placeholder') }}'"
-                        :options="{{ json_encode([]) }}"
-                    ></akaunting-search>
-                </div>
+    @if ($reconciliations->count())
+        <div class="card">
+            <div class="card-header border-bottom-0" :class="[{'bg-gradient-primary': bulk_action.show}]">
+                {!! Form::open([
+                    'method' => 'GET',
+                    'route' => 'reconciliations.index',
+                    'role' => 'form',
+                    'class' => 'mb-0'
+                ]) !!}
+                    <div class="align-items-center" v-if="!bulk_action.show">
+                        <akaunting-search
+                            :placeholder="'{{ trans('general.search_placeholder') }}'"
+                            :options="{{ json_encode([]) }}"
+                        ></akaunting-search>
+                    </div>
 
-                {{ Form::bulkActionRowGroup('general.reconciliations', $bulk_actions, ['group' => 'banking', 'type' => 'reconciliations']) }}
-            {!! Form::close() !!}
-        </div>
-
-        <div class="table-responsive">
-            <table class="table table-flush table-hover">
-                <thead class="thead-light">
-                    <tr class="row table-head-line">
-                        <th class="col-sm-2 col-md-1 col-lg-1 d-none d-sm-block">{{ Form::bulkActionAllGroup() }}</th>
-                        <th class="col-sm-3 col-md-2 col-lg-2 d-none d-sm-block">@sortablelink('created_at', trans('general.created_date'), ['filter' => 'active, visible'], ['class' => 'col-aka', 'rel' => 'nofollow'])</th>
-                        <th class="col-xs-4 col-sm-3 col-md-3 col-lg-3">@sortablelink('account_id', trans_choice('general.accounts', 1))</th>
-                        <th class="col-lg-2 d-none d-lg-block">{{ trans('general.period') }}</th>
-                        <th class="col-md-2 col-lg-2 d-none d-md-block text-right">@sortablelink('closing_balance', trans('reconciliations.closing_balance'))</th>
-                        <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1">@sortablelink('status', trans_choice('general.statuses', 1))</th>
-                        <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">{{ trans('general.actions') }}</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach($reconciliations as $item)
-                        <tr class="row align-items-center border-top-1">
-                            <td class="col-sm-2 col-md-1 col-lg-1 d-none d-sm-block">{{ Form::bulkActionGroup($item->id, $item->account->name) }}</td>
-                            <td class="col-sm-3 col-md-2 col-lg-2 d-none d-sm-block"><a class="col-aka" href="{{ route('reconciliations.edit', $item->id) }}">@date($item->created_at)</a></td>
-                            <td class="col-xs-4 col-sm-3 col-md-3 col-lg-3">{{ $item->account->name }}</td>
-                            <td class="col-lg-2 d-none d-lg-block border-0">@date($item->started_at) - @date($item->ended_at)</td>
-                            <td class="col-md-2 col-lg-2 d-none d-md-block text-right">@money($item->closing_balance, $item->account->currency_code, true)</td>
-                            <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1">
-                                @if ($item->reconciled)
-                                    <span class="badge badge-pill badge-success">{{ trans('reconciliations.reconciled') }}</span>
-                                @else
-                                    <span class="badge badge-pill badge-danger">{{ trans('reconciliations.unreconciled') }}</span>
-                                @endif
-                            </td>
-                            <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">
-                                <div class="dropdown">
-                                    <a class="btn btn-neutral btn-sm text-light items-align-center py-2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fa fa-ellipsis-h text-muted"></i>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                        <a class="dropdown-item" href="{{ route('reconciliations.edit', $item->id) }}">{{ trans('general.edit') }}</a>
-                                        @permission('delete-banking-reconciliations')
-                                            <div class="dropdown-divider"></div>
-                                            {!! Form::deleteLink($item, 'reconciliations.destroy') !!}
-                                        @endpermission
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        <div class="card-footer table-action">
-            <div class="row">
-                @include('partials.admin.pagination', ['items' => $reconciliations])
+                    {{ Form::bulkActionRowGroup('general.reconciliations', $bulk_actions, ['group' => 'banking', 'type' => 'reconciliations']) }}
+                {!! Form::close() !!}
             </div>
-       </div>
-    </div>
+
+            <div class="table-responsive">
+                <table class="table table-flush table-hover">
+                    <thead class="thead-light">
+                        <tr class="row table-head-line">
+                            <th class="col-sm-2 col-md-1 col-lg-1 d-none d-sm-block">{{ Form::bulkActionAllGroup() }}</th>
+                            <th class="col-sm-3 col-md-2 col-lg-2 d-none d-sm-block">@sortablelink('created_at', trans('general.created_date'), ['filter' => 'active, visible'], ['class' => 'col-aka', 'rel' => 'nofollow'])</th>
+                            <th class="col-xs-4 col-sm-3 col-md-3 col-lg-3">@sortablelink('account_id', trans_choice('general.accounts', 1))</th>
+                            <th class="col-lg-2 d-none d-lg-block">{{ trans('general.period') }}</th>
+                            <th class="col-md-2 col-lg-2 d-none d-md-block text-right">@sortablelink('closing_balance', trans('reconciliations.closing_balance'))</th>
+                            <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1">@sortablelink('status', trans_choice('general.statuses', 1))</th>
+                            <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">{{ trans('general.actions') }}</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach($reconciliations as $item)
+                            <tr class="row align-items-center border-top-1">
+                                <td class="col-sm-2 col-md-1 col-lg-1 d-none d-sm-block">{{ Form::bulkActionGroup($item->id, $item->account->name) }}</td>
+                                <td class="col-sm-3 col-md-2 col-lg-2 d-none d-sm-block"><a class="col-aka" href="{{ route('reconciliations.edit', $item->id) }}">@date($item->created_at)</a></td>
+                                <td class="col-xs-4 col-sm-3 col-md-3 col-lg-3">{{ $item->account->name }}</td>
+                                <td class="col-lg-2 d-none d-lg-block border-0">@date($item->started_at) - @date($item->ended_at)</td>
+                                <td class="col-md-2 col-lg-2 d-none d-md-block text-right">@money($item->closing_balance, $item->account->currency_code, true)</td>
+                                <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1">
+                                    @if ($item->reconciled)
+                                        <span class="badge badge-pill badge-success">{{ trans('reconciliations.reconciled') }}</span>
+                                    @else
+                                        <span class="badge badge-pill badge-danger">{{ trans('reconciliations.unreconciled') }}</span>
+                                    @endif
+                                </td>
+                                <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">
+                                    <div class="dropdown">
+                                        <a class="btn btn-neutral btn-sm text-light items-align-center py-2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fa fa-ellipsis-h text-muted"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                            <a class="dropdown-item" href="{{ route('reconciliations.edit', $item->id) }}">{{ trans('general.edit') }}</a>
+                                            @permission('delete-banking-reconciliations')
+                                                <div class="dropdown-divider"></div>
+                                                {!! Form::deleteLink($item, 'reconciliations.destroy') !!}
+                                            @endpermission
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card-footer table-action">
+                <div class="row">
+                    @include('partials.admin.pagination', ['items' => $reconciliations])
+                </div>
+        </div>
+        </div>
+    @else
+        @include('partials.admin.empty_page', ['page' => 'reconciliations', 'docs_path' => 'banking/reconciliations'])
+    @endif
 @endsection
 
 @push('scripts_start')
--- resources/views/settings/taxes/index.blade.php
+++ resources/views/settings/taxes/index.blade.php
@@ -9,82 +9,86 @@
 @endpermission
 
 @section('content')
-    <div class="card">
-        <div class="card-header border-bottom-0" :class="[{'bg-gradient-primary': bulk_action.show}]">
-            {!! Form::open([
-                'method' => 'GET',
-                'route' => 'taxes.index',
-                'role' => 'form',
-                'class' => 'mb-0'
-            ]) !!}
-                <div class="align-items-center" v-if="!bulk_action.show">
-                    <akaunting-search
-                        :placeholder="'{{ trans('general.search_placeholder') }}'"
-                        :options="{{ json_encode([]) }}"
-                    ></akaunting-search>
-                </div>
+    @if ($taxes->count())
+        <div class="card">
+            <div class="card-header border-bottom-0" :class="[{'bg-gradient-primary': bulk_action.show}]">
+                {!! Form::open([
+                    'method' => 'GET',
+                    'route' => 'taxes.index',
+                    'role' => 'form',
+                    'class' => 'mb-0'
+                ]) !!}
+                    <div class="align-items-center" v-if="!bulk_action.show">
+                        <akaunting-search
+                            :placeholder="'{{ trans('general.search_placeholder') }}'"
+                            :options="{{ json_encode([]) }}"
+                        ></akaunting-search>
+                    </div>
 
-                {{ Form::bulkActionRowGroup('general.taxes', $bulk_actions, ['group' => 'settings', 'type' => 'taxes']) }}
-            {!! Form::close() !!}
-        </div>
+                    {{ Form::bulkActionRowGroup('general.taxes', $bulk_actions, ['group' => 'settings', 'type' => 'taxes']) }}
+                {!! Form::close() !!}
+            </div>
 
-        <div class="table-responsive">
-            <table class="table table-flush table-hover">
-                <thead class="thead-light">
-                    <tr class="row table-head-line">
-                        <th class="col-sm-2 col-md-2 col-lg-1 d-none d-sm-block">{{ Form::bulkActionAllGroup() }}</th>
-                        <th class="col-xs-4 col-sm-3 col-md-2 col-lg-4">@sortablelink('name', trans('general.name'), ['filter' => 'active, visible'], ['class' => 'col-aka', 'rel' => 'nofollow'])</th>
-                        <th class="col-md-2 col-lg-2 d-none d-md-block">@sortablelink('rate', trans('taxes.rate_percent'))</th>
-                        <th class="col-sm-2 col-md-2 col-lg-2 d-none d-sm-block">@sortablelink('type', trans_choice('general.types', 1))</th>
-                        <th class="col-xs-4 col-sm-3 col-md-2 col-lg-2">@sortablelink('enabled', trans('general.enabled'))</th>
-                        <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">{{ trans('general.actions') }}</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach($taxes as $item)
-                        <tr class="row align-items-center border-top-1">
-                            <td class="col-sm-2 col-md-2 col-lg-1 d-none d-sm-block">{{ Form::bulkActionGroup($item->id, $item->name) }}</td>
-                            <td class="col-xs-4 col-sm-3 col-md-2 col-lg-4"><a class="col-aka" href="{{ route('taxes.edit', $item->id) }}">{{ $item->name }}</a></td>
-                            <td class="col-md-2 col-lg-2 d-none d-md-block">{{ $item->rate }}</td>
-                            <td class="col-sm-2 col-md-2 col-lg-2 d-none d-sm-block">{{ $types[$item->type] }}</td>
-                            <td class="col-xs-4 col-sm-3 col-md-2 col-lg-2">
-                                @if (user()->can('update-settings-taxes'))
-                                    {{ Form::enabledGroup($item->id, $item->name, $item->enabled) }}
-                                @else
-                                    @if ($item->enabled)
-                                        <badge rounded type="success">{{ trans('general.enabled') }}</badge>
-                                    @else
-                                        <badge rounded type="danger">{{ trans('general.disabled') }}</badge>
-                                    @endif
-                                @endif
-                            </td>
-                            <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">
-                                <div class="dropdown">
-                                    <a class="btn btn-neutral btn-sm text-light items-align-center py-2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fa fa-ellipsis-h text-muted"></i>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                        <a class="dropdown-item" href="{{ route('taxes.edit', $item->id) }}">{{ trans('general.edit') }}</a>
-                                        @permission('delete-settings-taxes')
-                                            <div class="dropdown-divider"></div>
-                                            {!! Form::deleteLink($item, 'taxes.destroy', 'tax_rates') !!}
-                                        @endpermission
-                                    </div>
-                                </div>
-                            </td>
+            <div class="table-responsive">
+                <table class="table table-flush table-hover">
+                    <thead class="thead-light">
+                        <tr class="row table-head-line">
+                            <th class="col-sm-2 col-md-2 col-lg-1 d-none d-sm-block">{{ Form::bulkActionAllGroup() }}</th>
+                            <th class="col-xs-4 col-sm-3 col-md-2 col-lg-4">@sortablelink('name', trans('general.name'), ['filter' => 'active, visible'], ['class' => 'col-aka', 'rel' => 'nofollow'])</th>
+                            <th class="col-md-2 col-lg-2 d-none d-md-block">@sortablelink('rate', trans('taxes.rate_percent'))</th>
+                            <th class="col-sm-2 col-md-2 col-lg-2 d-none d-sm-block">@sortablelink('type', trans_choice('general.types', 1))</th>
+                            <th class="col-xs-4 col-sm-3 col-md-2 col-lg-2">@sortablelink('enabled', trans('general.enabled'))</th>
+                            <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">{{ trans('general.actions') }}</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
 
-        <div class="card-footer table-action">
-            <div class="row">
-                @include('partials.admin.pagination', ['items' => $taxes])
+                    <tbody>
+                        @foreach($taxes as $item)
+                            <tr class="row align-items-center border-top-1">
+                                <td class="col-sm-2 col-md-2 col-lg-1 d-none d-sm-block">{{ Form::bulkActionGroup($item->id, $item->name) }}</td>
+                                <td class="col-xs-4 col-sm-3 col-md-2 col-lg-4"><a class="col-aka" href="{{ route('taxes.edit', $item->id) }}">{{ $item->name }}</a></td>
+                                <td class="col-md-2 col-lg-2 d-none d-md-block">{{ $item->rate }}</td>
+                                <td class="col-sm-2 col-md-2 col-lg-2 d-none d-sm-block">{{ $types[$item->type] }}</td>
+                                <td class="col-xs-4 col-sm-3 col-md-2 col-lg-2">
+                                    @if (user()->can('update-settings-taxes'))
+                                        {{ Form::enabledGroup($item->id, $item->name, $item->enabled) }}
+                                    @else
+                                        @if ($item->enabled)
+                                            <badge rounded type="success">{{ trans('general.enabled') }}</badge>
+                                        @else
+                                            <badge rounded type="danger">{{ trans('general.disabled') }}</badge>
+                                        @endif
+                                    @endif
+                                </td>
+                                <td class="col-xs-4 col-sm-2 col-md-2 col-lg-1 text-center">
+                                    <div class="dropdown">
+                                        <a class="btn btn-neutral btn-sm text-light items-align-center py-2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fa fa-ellipsis-h text-muted"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                            <a class="dropdown-item" href="{{ route('taxes.edit', $item->id) }}">{{ trans('general.edit') }}</a>
+                                            @permission('delete-settings-taxes')
+                                                <div class="dropdown-divider"></div>
+                                                {!! Form::deleteLink($item, 'taxes.destroy', 'tax_rates') !!}
+                                            @endpermission
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card-footer table-action">
+                <div class="row">
+                    @include('partials.admin.pagination', ['items' => $taxes])
+                </div>
             </div>
         </div>
-    </div>
+    @else
+        @include('partials.admin.empty_page', ['page' => 'taxes', 'docs_path' => 'settings/taxes'])
+    @endif
 @endsection
 
 @push('scripts_start')
