d36b6a55e929d9dbbf027ada2dc6741274bac904
batuhanbas <b1atuhanb1as@gmail.com>
2020-02-17 16:22:29 +0300

    footer text color updated

--- resources/views/partials/admin/footer.blade.php
+++ resources/views/partials/admin/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <div class="text-sm float-left text-muted footer-texts">
-                    {{ trans('footer.powered') }}: <a href="{{ trans('footer.link') }}" target="_blank">{{ trans('footer.software') }}</a>
+                    {{ trans('footer.powered') }}: <a href="{{ trans('footer.link') }}" target="_blank" class="text-muted">{{ trans('footer.software') }}</a>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6">
