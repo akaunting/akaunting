4938dceef08ca80222aa4c0d82a3535f18d8287e
denisdulici <denisdulici@gmail.com>
2020-03-15 01:45:26 +0300

    add new env variable

--- app/Providers/Event.php
+++ app/Providers/Event.php
@@ -17,6 +17,7 @@ class Event extends Provider
             'App\Listeners\Update\V20\Version200',
             'App\Listeners\Update\V20\Version203',
             'App\Listeners\Update\V20\Version205',
+            'App\Listeners\Update\V20\Version207',
         ],
         'Illuminate\Auth\Events\Login' => [
             'App\Listeners\Auth\Login',
