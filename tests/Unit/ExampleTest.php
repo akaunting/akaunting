af87c2d40011db7ecdfae3ea013b877448a5ca8a
denisdulici <denisdulici@gmail.com>
2020-03-14 23:38:54 +0300

    updated test suite

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,9 +2,7 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
