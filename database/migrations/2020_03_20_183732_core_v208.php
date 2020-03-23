2019f1c779ed81303e87ac496b38d71132939079
Burak Çakırel <burakcakirel@gmail.com>
2020-03-24 02:03:10 +0300

    Rename migration class

--- database/migrations/2020_03_20_183732_core_v208.php
+++ database/migrations/2020_03_20_183732_core_v208.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDiscountColumnsInvoiceItemsTable extends Migration
+class CoreV208 extends Migration
 {
     /**
      * Run the migrations.
