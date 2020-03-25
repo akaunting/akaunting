da5f7f5d5e6d212fe59d05bec20f8f8db5e063e4
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-03-25 16:23:19 +0300

    close #1379 Fixed: App delete link show wrong name

--- resources/views/partials/form/delete_button.blade.php
+++ resources/views/partials/form/delete_button.blade.php
@@ -10,11 +10,22 @@
     }
 
     $text = $text ? $text : $page;
+
+    $title = trans_choice('general.' . $text, 2);
+    $type = mb_strtolower(trans_choice('general.' . $text, 1));
+
+    // for module
+    if (\Str::contains($text, ['::'])) {
+        $title = trans_choice($text, 2);
+        $type = mb_strtolower(trans_choice($text, 1)); 
+    }
+
+    $message = trans('general.delete_confirm', ['name' => '<strong>' . $name . '</strong>', 'type' => $type]);
 @endphp
 
 {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ' . trans('general.delete'), array(
     'type'    => 'button',
     'class'   => 'btn btn-danger btn-xs',
     'title'   => trans('general.delete'),
-    '@click'  => 'confirmDelete("' . $url . '", "' . trans_choice('general.' . $text, 2) . '", "' . trans('general.delete_confirm', ['name' => '<strong>' . $item->$value . '</strong>', 'type' => mb_strtolower(trans_choice('general.' . $text, 1))]) . '", "' . trans('general.cancel') . '", "' . trans('general.delete') . '")'
+    '@click'  => 'confirmDelete("' . $url . '", "' . $title . '", "' . $message . '", "' . trans('general.cancel') . '", "' . trans('general.delete') . '")'
 )) !!}
--- resources/views/partials/form/delete_link.blade.php
+++ resources/views/partials/form/delete_link.blade.php
@@ -12,11 +12,22 @@
     $text = $text ? $text : $page;
 
     $name = addslashes($item->$value);
+
+    $title = trans_choice('general.' . $text, 2);
+    $type = mb_strtolower(trans_choice('general.' . $text, 1));
+
+    // for module
+    if (\Str::contains($text, ['::'])) {
+        $title = trans_choice($text, 2);
+        $type = mb_strtolower(trans_choice($text, 1)); 
+    }
+
+    $message = trans('general.delete_confirm', ['name' => '<strong>' . $name . '</strong>', 'type' => $type]);
 @endphp
 
 {!! Form::button(trans('general.delete'), array(
     'type'    => 'button',
     'class'   => 'dropdown-item action-delete',
     'title'   => trans('general.delete'),
-    '@click'  => 'confirmDelete("' . $url . '", "' . trans_choice('general.' . $text, 2) . '", "' . trans('general.delete_confirm', ['name' => '<strong>' . $name . '</strong>', 'type' => mb_strtolower(trans_choice('general.' . $text, 1))]) . '", "' . trans('general.cancel') . '", "' . trans('general.delete') . '")'
+    '@click'  => 'confirmDelete("' . $url . '", "' . $title . '", "' . $message . '", "' . trans('general.cancel') . '", "' . trans('general.delete') . '")'
 )) !!}
