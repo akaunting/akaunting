d94bf5d5b52b673ee6ed85ca17025b40cb4e8f11
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-04-07 10:56:45 +0300

    version update 2.0.7 to 2.0.8

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '0',
 
-    'patch'     =>  '7',
+    'patch'     =>  '8',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '20-Mar-2020',
+    'date'      =>  '07-April-2020',
 
-    'time'      =>  '20:30',
+    'time'      =>  '11:00',
 
     'zone'      =>  'GMT +3',
 
