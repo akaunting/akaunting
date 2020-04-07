663b1a5d6ac27870fb89b482b57e84d88d8a9cc5
denisdulici <denisdulici@gmail.com>
2020-04-07 14:48:25 +0300

    applied new scopes to factories

--- database/factories/Bill.php
+++ database/factories/Bill.php
@@ -23,9 +23,7 @@ $factory->define(Bill::class, function (Faker $faker) use ($company) {
     $billed_at = $faker->dateTimeBetween(now()->startOfYear(), now()->endOfYear())->format('Y-m-d');
     $due_at = Date::parse($billed_at)->addDays(10)->format('Y-m-d');
 
-    $types = (string) setting('contact.type.vendor', 'vendor');
-
-    $contacts = Contact::type(explode(',', $types))->enabled()->get();
+    $contacts = Contact::vendor()->enabled()->get();
 
     if ($contacts->count()) {
         $contact = $contacts->random(1)->first();
--- database/factories/Invoice.php
+++ database/factories/Invoice.php
@@ -24,9 +24,7 @@ $factory->define(Invoice::class, function (Faker $faker) use ($company) {
     $invoiced_at = $faker->dateTimeBetween(now()->startOfYear(), now()->endOfYear())->format('Y-m-d');
     $due_at = Date::parse($invoiced_at)->addDays(setting('invoice.payment_terms'))->format('Y-m-d');
 
-    $types = (string) setting('contact.type.customer', 'customer');
-
-    $contacts = Contact::type(explode(',', $types))->enabled()->get();
+    $contacts = Contact::customer()->enabled()->get();
 
     if ($contacts->count()) {
         $contact = $contacts->random(1)->first();
