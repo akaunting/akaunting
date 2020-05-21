e2352f41b5d6d9367049933712af58be8a7ac602
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-05-21 22:23:02 +0300

    version update 2.0.11 to 2.0.12

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '0',
 
-    'patch'     =>  '11',
+    'patch'     =>  '12',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '2-May-2020',
+    'date'      =>  '21-May-2020',
 
-    'time'      =>  '21:00',
+    'time'      =>  '19:00',
 
     'zone'      =>  'GMT +3',
 
