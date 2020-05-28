ad51d61baa003b323369517054be29eb35611e1b
denisdulici <denisdulici@gmail.com>
2020-05-28 03:07:20 +0300

    make sure module exists

--- app/Utilities/Updater.php
+++ app/Utilities/Updater.php
@@ -164,7 +164,17 @@ class Updater
         $versions = Versions::all($modules);
 
         foreach ($versions as $alias => $latest_version) {
-            $installed_version = ($alias == 'core') ? version('short') : module($alias)->get('version');
+            if ($alias == 'core') {
+                $installed_version = version('short');
+            } else {
+                $module = module($alias);
+
+                if (!$module instanceof \Akaunting\Module\Module) {
+                    continue;
+                }
+
+                $installed_version = $module->get('version');
+            }
 
             if (version_compare($installed_version, $latest_version, '>=')) {
                 continue;
