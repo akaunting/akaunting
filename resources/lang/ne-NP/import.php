dc2f572ce4846903b6eba34d45a21751e32f83d4
Denis Duliçi <denisdulici@gmail.com>
2020-06-04 14:13:40 +0300

    ne-np

---
+++ resources/lang/ne-NP/import.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'import'                => 'आयात गर्नुहोस',
+    'title'                 => 'आयात :type',
+    'message'               => 'अनुमति प्राप्त फाइल का प्रकार हरू: XLS, XLSX. कृपया, Sample फाइल<a target="_blank" href=":link"><strong>डाउनलोड</strong></a> गर्नुहोस।',
+
+];
---
+++ resources/lang/ne-NP/transfers.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'from_account'          => 'खाताबाट',
+    'to_account'            => 'खातामा',
+
+    'messages' => [
+        'delete'            => ':from बाट :to (:amount)',
+    ],
+
+];
