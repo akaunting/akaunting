e9f7b1ad8d3760e8051e13cb5ccc0ac1a086a4c1
Burak Çakırel <burakcakirel@gmail.com>
2020-06-04 16:34:34 +0300

    Show dynamic component also in portal

--- resources/views/partials/portal/content.blade.php
+++ resources/views/partials/portal/content.blade.php
@@ -6,5 +6,8 @@
 
         @stack('content_content_end')
         <notifications></notifications>
+
+        <form id="form-create" method="POST" action="#"/>
+        <component v-bind:is="component"></component>
     </div>
 @stack('content_end')
