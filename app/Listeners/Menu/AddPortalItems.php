08b68052daf5cefb82a6f63f2677c4b0d6fa8b51
Denis Duliçi <denisdulici@gmail.com>
2020-06-24 12:39:01 +0300

    re-ordered menu items

--- app/Listeners/Menu/AddPortalItems.php
+++ app/Listeners/Menu/AddPortalItems.php
@@ -17,14 +17,14 @@ class AddPortalItems
         $user = user();
         $menu = $event->menu;
 
-        $menu->route('portal.dashboard', trans_choice('general.dashboards', 1), [], 100, ['icon' => 'fa fa-tachometer-alt']);
+        $menu->route('portal.dashboard', trans_choice('general.dashboards', 1), [], 10, ['icon' => 'fa fa-tachometer-alt']);
 
         if ($user->can('read-portal-invoices')) {
-            $menu->route('portal.invoices.index', trans_choice('general.invoices', 2), [], 200, ['icon' => 'fa fa-file-signature']);
+            $menu->route('portal.invoices.index', trans_choice('general.invoices', 2), [], 20, ['icon' => 'fa fa-file-signature']);
         }
 
         if ($user->can('read-portal-payments')) {
-            $menu->route('portal.payments.index', trans_choice('general.payments', 2), [], 300, ['icon' => 'fa fa-money-bill']);
+            $menu->route('portal.payments.index', trans_choice('general.payments', 2), [], 30, ['icon' => 'fa fa-money-bill']);
         }
     }
 }
