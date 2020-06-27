1f5901d3c73935fd11e76c15852841c18a4bf919
Denis Duliçi <denisdulici@gmail.com>
2020-06-28 00:24:03 +0300

    added php version control to console

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,20 +1,17 @@
 <?php
 
+// Define minimum supported PHP version
+define('AKAUNTING_PHP', '7.2.5');
+
+// Check PHP version
+if (version_compare(PHP_VERSION, AKAUNTING_PHP, '<')) {
+    die('ERROR: Your host needs to use PHP ' . AKAUNTING_PHP . ' or higher to run Akaunting');
+}
+
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Register The Composer Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
+// Load composer for core
+require __DIR__ . '/../vendor/autoload.php';
 
 // Load composer for modules
 foreach (glob(__DIR__ . '/../modules/*') as $folder) {
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Akaunting
  * @copyright   2017-2020 Akaunting. All rights reserved.
@@ -6,19 +7,11 @@
  * @link        https://akaunting.com
  */
 
-// Define minimum supported PHP version
-define('AKAUNTING_PHP', '7.2.5');
-
-// Check PHP version
-if (version_compare(PHP_VERSION, AKAUNTING_PHP, '<')) {
-    die('Your host needs to use PHP ' . AKAUNTING_PHP . ' or higher to run Akaunting');
-}
-
 // Register the auto-loader
-require(__DIR__.'/bootstrap/autoload.php');
+require(__DIR__ . '/bootstrap/autoload.php');
 
 // Load the app
-$app = require_once(__DIR__.'/bootstrap/app.php');
+$app = require_once(__DIR__ . '/bootstrap/app.php');
 
 // Run the app
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
