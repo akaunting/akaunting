bd07382dd6421512aa33f60cee6cf027747cce64
Denis Duliçi <denisdulici@gmail.com>
2020-07-15 14:14:20 +0300

    fixed #1533

--- app/Abstracts/Http/Controller.php
+++ app/Abstracts/Http/Controller.php
@@ -44,14 +44,18 @@ abstract class Controller extends BaseController
 
         $controller = '';
 
+        // Add module
+        if (isset($arr[3]) && isset($arr[4])) {
+            if (strtolower($arr[4]) == 'modules') {
+                $controller .= Str::kebab($arr[3]) . '-';
+            } elseif (isset($arr[5]) && (strtolower($arr[5]) == 'modules')) {
+                $controller .= Str::kebab($arr[4]) . '-';
+            }
+        }
+
         // Add folder
         if (strtolower($arr[1]) != 'controllers') {
             $controller .= Str::kebab($arr[1]) . '-';
-        }
-
-        // Add module
-        if (isset($arr[3]) && isset($arr[4]) && (strtolower($arr[4]) == 'modules')) {
-            $controller .= Str::kebab($arr[3]) . '-';
         }
 
         // Add file
@@ -62,6 +66,11 @@ abstract class Controller extends BaseController
         if (in_array($controller, $skip)) {
             return;
         }
+
+        // App\Http\Controllers\FooBar                  -->> foo-bar
+        // App\Http\Controllers\FooBar\Main             -->> foo-bar-main
+        // Modules\Blog\Http\Controllers\Posts          -->> blog-posts
+        // Modules\Blog\Http\Controllers\Portal\Posts   -->> blog-portal-posts
 
         // Add CRUD permission check
         $this->middleware('permission:create-' . $controller)->only('create', 'store', 'duplicate', 'import');
