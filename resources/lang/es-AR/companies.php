f0a4fe0776f51b7ce4f8c9f17dea39985d81a319
Denis Duliçi <denisdulici@gmail.com>
2020-07-23 15:36:51 +0300

    es-AR translation

---
+++ resources/lang/es-AR/companies.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'domain'                => 'Dominio',
+    'logo'                  => 'Logo',
+
+    'error' => [
+        'not_user_company'  => 'Error: ¡No tiene permiso para cambiar esta empresa!',
+        'delete_active'     => 'Error: No se puede eliminar la empresa activa. Por favor, cambie a otra primero!',
+        'disable_active'    => 'Error: No se puede desactivar la empresa activa. ¡Por favor, cambie a otra primero!',
+    ],
+
+];
---
+++ resources/lang/es-AR/reconciliations.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'reconcile'             => 'Conciliar',
+    'reconciled'            => 'Conciliado',
+    'closing_balance'       => 'Balance de cierre',
+    'unreconciled'          => 'Sin conciliar',
+    'transactions'          => 'Transacciones',
+    'start_date'            => 'Fecha de inicio',
+    'end_date'              => 'Fecha de fin',
+    'cleared_amount'        => 'Monto',
+    'deposit'               => 'Depósito',
+    'withdrawal'            => 'Retiro',
+
+];
