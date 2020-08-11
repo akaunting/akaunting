d415cd6ae8a9c4ad69899e75b0660dd6104c5206
Denis Duliçi <denisdulici@gmail.com>
2020-08-12 02:33:40 +0300

    catch process exceptions

--- app/Utilities/Console.php
+++ app/Utilities/Console.php
@@ -3,29 +3,48 @@
 namespace App\Utilities;
 
 use Illuminate\Console\Application;
+use Illuminate\Support\Str;
+use Symfony\Component\Process\Exception\InvalidArgumentException;
+use Symfony\Component\Process\Exception\LogicException;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\Process;
 
 class Console
 {
-    public static function run($string, $all_output = false, $timeout = 0)
+    public static function run($string, $timeout = 0)
     {
         $command = Application::formatCommandString($string);
 
         logger('Console command:: ' . $command);
 
-        $process = Process::fromShellCommandline($command, base_path());
-        $process->setTimeout($timeout);
+        try {
+            $process = Process::fromShellCommandline($command, base_path());
+            $process->setTimeout($timeout);
 
-        $process->run();
+            $process->mustRun();
 
-        if ($process->isSuccessful()) {
-            return true;
+            $output = $process->getOutput();
+
+            if (static::isValidOutput($output)) {
+                return true;
+            }
+        } catch (InvalidArgumentException | LogicException | ProcessFailedException | RuntimeException $e) {
+            $output = $e->getMessage();
         }
-
-        $output = $all_output ? $process->getOutput() : $process->getErrorOutput();
 
         logger('Console output:: ' . $output);
 
         return $output;
+    }
+
+    public static function isValidOutput($output)
+    {
+        $errors = [
+            'Content-Type: application/json',
+            'CSRF token mismatch',
+        ];
+
+        return !Str::contains($output, $errors);
     }
 }
