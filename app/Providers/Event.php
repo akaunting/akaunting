34d781e6194c8e39782f84e8bfc653c9f0b0c828
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-09-23 12:13:04 +0300

    updated 2.0.23 version after run migrate

--- app/Providers/Event.php
+++ app/Providers/Event.php
@@ -23,6 +23,7 @@ class Event extends Provider
             'App\Listeners\Update\V20\Version2014',
             'App\Listeners\Update\V20\Version2017',
             'App\Listeners\Update\V20\Version2020',
+            'App\Listeners\Update\V20\Version2023',
         ],
         'Illuminate\Auth\Events\Login' => [
             'App\Listeners\Auth\Login',
