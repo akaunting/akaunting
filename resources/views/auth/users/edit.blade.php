d7af10fe549f7c784d15c924f2cf7160df597efa
Cüneyt Şentürk <cneytsenturk@gmail.com>
2020-09-23 11:31:24 +0300

    close #1611 Fixed: Not able to login using customer email

--- resources/views/auth/users/edit.blade.php
+++ resources/views/auth/users/edit.blade.php
@@ -49,7 +49,7 @@
                     @endif
 
                     @permission('read-common-companies')
-                    {{ Form::multiSelectRemoteGroup('companies', trans_choice('general.companies', 2), 'user', $companies, $user->company_ids, ['required' => 'required', 'remote_action' => route('companies.autocomplete'), 'remote_type' => 'company']) }}
+                        {{ Form::multiSelectRemoteGroup('companies', trans_choice('general.companies', 2), 'user', $companies, $user->company_ids, ['required' => 'required', 'disabled' => (in_array('customer', $user->roles()->pluck('name')->toArray())) ? 'true' : 'false', 'remote_action' => route('companies.autocomplete'), 'remote_type' => 'company']) }}
                     @endpermission
 
                     @permission('read-auth-roles')
