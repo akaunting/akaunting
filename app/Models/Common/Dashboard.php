86de28d87dfc39245999d6aa79460235462934db
Denis Duliçi <denisdulici@gmail.com>
2020-10-16 23:35:26 +0300

    fixed dashboard factory

--- app/Models/Common/Dashboard.php
+++ app/Models/Common/Dashboard.php
@@ -4,10 +4,11 @@ namespace App\Models\Common;
 
 use App\Abstracts\Model;
 use Bkwld\Cloner\Cloneable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Dashboard extends Model
 {
-    use Cloneable;
+    use Cloneable, HasFactory;
 
     protected $table = 'dashboards';
 
@@ -33,5 +34,15 @@ class Dashboard extends Model
     public function widgets()
     {
         return $this->hasMany('App\Models\Common\Widget')->orderBy('sort', 'asc');
+    }
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return \Database\Factories\Dashboard::new();
     }
 }
