2666c67e2d2c3e8b3fc03c0439fa458ee9689576
Steven Brauer <steven.brauer@webbrauer.ch>
2020-11-15 18:10:41 +0100

    Update SendInvoiceRecurringNotification.php

--- app/Listeners/Sale/SendInvoiceRecurringNotification.php
+++ app/Listeners/Sale/SendInvoiceRecurringNotification.php
@@ -16,9 +16,10 @@ class SendInvoiceRecurringNotification
     public function handle(Event $event)
     {
         $invoice = $event->invoice;
+        $customer = $invoice->contact()->first();
 
         // Notify the customer
-        if ($invoice->contact && !empty($invoice->contact_email)) {
+        if ($invoice->contact && !empty($invoice->contact_email) && ($customer->send_new_invoice == 1)) {
             $invoice->contact->notify(new Notification($invoice, 'invoice_recur_customer'));
         }
 
