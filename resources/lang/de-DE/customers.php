a5e02ef87efd8e1d3132c7a230b322e658cd6543
Steven Brauer <steven.brauer@webbrauer.ch>
2020-11-15 18:12:55 +0100

    Update customers.php

--- resources/lang/de-DE/customers.php
+++ resources/lang/de-DE/customers.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    
+    'notifications' => [
+        'label'                 => 'Automatische  Benachrichtigungen (per E-Mail)',
+        'send_new_invoice'      => 'Kunde automatisch benachrichtigen wenn eine neue Rechnung erstellt wurde?',
+        'send_remind_invoice'   => 'Kunde automatisch benachrichtigen wenn die Rechnung fällig ist?',
+        'send_payment_received' => 'Kunde automatisch benachrichtigen wenn Zahlung eingegangen ist?',
+    ],
 
     'can_login'             => 'Login erlauben?',
     'user_created'          => 'Benutzer angelegt',
