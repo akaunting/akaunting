5b2e24ee5144605a0bc57d169eab59ff9fa18d27
Denis Duliçi <denisdulici@gmail.com>
2020-11-18 13:19:24 +0300

    fixed dingo api conflict

--- app/Abstracts/Http/ApiController.php
+++ app/Abstracts/Http/ApiController.php
@@ -2,13 +2,27 @@
 
 namespace App\Abstracts\Http;
 
+use App\Traits\Jobs;
+use App\Traits\Permissions;
+use App\Traits\Relationships;
 use Dingo\Api\Exception\ResourceException;
 use Dingo\Api\Routing\Helpers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
 
-abstract class ApiController extends Controller
+abstract class ApiController extends BaseController
 {
-    use Helpers;
+    use AuthorizesRequests, Jobs, Helpers, Permissions, Relationships, ValidatesRequests;
+
+    /**
+     * Instantiate a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->assignPermissionsToController();
+    }
 
     /**
      * Create the response for when a request fails validation.
