bdba2b0ab73451e2d321d9ce1ded0d4a2206c3e2
flufftronix <michael@flufftronix.com>
2020-11-28 16:55:34 -0500

    Create customers.php

---
+++ resources/lang/en-US/customers.php
@@ -0,0 +1,13 @@
+
+<?php
+
+return [
+
+    'can_login'             => 'Can Login?',
+    'user_created'          => 'User Created',
+
+    'error' => [
+        'email'             => 'The email has already been taken.',
+    ],
+
+];
