eb844903b7b934e306d194798143e1d8ba73c040
mathewcallaghan <mathewcallaghan@gmail.com>
2020-12-03 13:29:04 +1100

    Adding en-AU

---
+++ resources/lang/en-AU/currencies.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'code'                  => 'Code',
+    'rate'                  => 'Rate',
+    'default'               => 'Default Currency',
+    'decimal_mark'          => 'Decimal Mark',
+    'thousands_separator'   => 'Thousands Separator',
+    'precision'             => 'Precision',
+    'symbol' => [
+        'symbol'            => 'Symbol',
+        'position'          => 'Symbol Position',
+        'before'            => 'Before Amount',
+        'after'             => 'After Amount',
+    ]
+
+];
