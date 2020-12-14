348e957735db5819de8c60fefef2250539974b6f
Denis Duliçi <denisdulici@gmail.com>
2020-12-14 15:11:38 +0300

    updated laratrust version

--- app/Providers/Auth.php
+++ app/Providers/Auth.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as Provider;
 
 class Auth extends Provider
@@ -25,12 +23,5 @@ class Auth extends Provider
     public function boot()
     {
         $this->registerPolicies();
-
-        // Register permissions to Laravel Gate
-        app(Gate::class)->before(function (Authorizable $user, string $ability) {
-            if (method_exists($user, 'hasPermission')) {
-                return $user->hasPermission($ability) ?: null;
-            }
-        });
     }
 }
