da5b617537288f95ec9a142f920cfdf9c05934e6
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-01-11 12:33:00 +0300

    timeline create text fixed..

--- app/Abstracts/View/Components/DocumentShow.php
+++ app/Abstracts/View/Components/DocumentShow.php
@@ -1052,7 +1052,15 @@ abstract class DocumentShow extends Base
             return $textTimelineCreateTitle;
         }
 
-        $default_key = 'create_' . $type;
+        $default_key = 'create_' . str_replace('-', '_', $type);
+
+        $translation = $this->getTextFromConfig($type, 'timeline_create_title', $default_key);
+
+        if (!empty($translation)) {
+            return $translation;
+        }
+
+        $default_key = 'create_' . str_replace('-', '_', config('type.' . $type . '.alias');
 
         $translation = $this->getTextFromConfig($type, 'timeline_create_title', $default_key);
 
