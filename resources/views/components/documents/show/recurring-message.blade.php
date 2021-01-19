cf27f79e31db14581ebd1438d1987653b87df095
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-01-19 18:32:26 +0300

    recurring message styling..

--- resources/views/components/documents/show/recurring-message.blade.php
+++ resources/views/components/documents/show/recurring-message.blade.php
@@ -1,25 +1,21 @@
 @stack('recurring_message_start')
 @if (($recurring = $document->recurring) && ($next = $recurring->getNextRecurring()))
-    <div class="row mb-3">
+    <div class="row">
         <div class="col-sm-12">
-            <div class="media">
-                <div class="media-body">
-                    <div class="media-comment-text">
-                        <div class="d-flex">
-                            @stack('recurring_message_head_start')
-                                <h5 class="mt-0">{{ trans('recurring.recurring') }}</h5>
-                            @stack('recurring_message_head_end')
-                        </div>
-
-                        @stack('recurring_message_body_start')
-                            <p class="text-sm lh-160 mb-0">{{ trans('recurring.message', [
-                                'type' => mb_strtolower(trans_choice($textRecurringType, 1)),
-                                'date' => $next->format($date_format)
-                            ]) }}
-                            </p>
-                        @stack('recurring_message_body_end')
-                    </div>
+            <div class="alert alert-info fade show" role="alert">
+                <div class="d-flex">
+                    @stack('recurring_message_head_start')
+                        <h5 class="mt-0 text-white"><strong>{{ trans('recurring.recurring') }}</strong></h5>
+                    @stack('recurring_message_head_end')
                 </div>
+
+                @stack('recurring_message_body_start')
+                    <p class="text-sm lh-160 mb-0">{{ trans('recurring.message', [
+                        'type' => mb_strtolower(trans_choice($textRecurringType, 1)),
+                        'date' => $next->format($date_format)
+                    ]) }}
+                    </p>
+                @stack('recurring_message_body_end')
             </div>
         </div>
     </div>
