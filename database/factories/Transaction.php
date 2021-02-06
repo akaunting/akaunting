f399b8a4134e746da51e1cb0b021c2275d0f0ddf
Denis Duliçi <denisdulici@gmail.com>
2021-02-07 01:12:57 +0300

    more styling

--- database/factories/Transaction.php
+++ database/factories/Transaction.php
@@ -8,7 +8,7 @@ use App\Traits\Transactions;
 
 class Transaction extends Factory
 {
-	use Transactions;
+    use Transactions;
 
     /**
      * The name of the factory's corresponding model.
@@ -29,19 +29,19 @@ class Transaction extends Factory
 
         $category_type = in_array($type, $this->getIncomeTypes()) ? 'income' : 'expense';
 
-		return [
-			'company_id' => $this->company->id,
-			'type' => $type,
-			'account_id' => setting('default.account'),
-			'paid_at' => $this->faker->dateTimeBetween(now()->startOfYear(), now()->endOfYear())->format('Y-m-d H:i:s'),
-			'amount' => $this->faker->randomFloat(2, 1, 1000),
-			'currency_code' => setting('default.currency'),
-			'currency_rate' => '1.0',
-			'description' => $this->faker->text(5),
-			'category_id' => $this->company->categories()->$category_type()->get()->random(1)->pluck('id')->first(),
-			'reference' => $this->faker->text(5),
-			'payment_method' => setting('default.payment_method'),
-		];
+        return [
+            'company_id' => $this->company->id,
+            'type' => $type,
+            'account_id' => setting('default.account'),
+            'paid_at' => $this->faker->dateTimeBetween(now()->startOfYear(), now()->endOfYear())->format('Y-m-d H:i:s'),
+            'amount' => $this->faker->randomFloat(2, 1, 1000),
+            'currency_code' => setting('default.currency'),
+            'currency_rate' => '1.0',
+            'description' => $this->faker->text(5),
+            'category_id' => $this->company->categories()->$category_type()->get()->random(1)->pluck('id')->first(),
+            'reference' => $this->faker->text(5),
+            'payment_method' => setting('default.payment_method'),
+        ];
     }
 
     /**
