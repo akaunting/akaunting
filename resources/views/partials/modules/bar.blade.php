0bb114de7123f98376cbd092712148562ed256f7
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-02-27 14:26:11 +0300

    apps page bar styling..

--- resources/views/partials/modules/bar.blade.php
+++ resources/views/partials/modules/bar.blade.php
@@ -6,8 +6,8 @@
                     <div class="col-xs-12 col-sm-2 pl-0 pr-0">
                         <akaunting-select
                             class="form-control-sm d-inline-block w-100"
-                            :placeholder="'{{ trans('general.form.select.field', ['field' => trans_choice('general.categories', 1)]) }}'"
-                            :name="'category'"
+                            placeholder="{{ trans('general.form.select.field', ['field' => trans_choice('general.categories', 1)]) }}"
+                            name="category"
                             :icon="''"
                             :options="{{ json_encode($categories) }}"
                             :value="'{{ request('category') }}'"
@@ -15,7 +15,9 @@
                         ></akaunting-select>
                         {{ Form::hidden('category_page', url("apps/categories"), ['id' => 'category_page']) }}
                     </div>
+
                     <div class="vr d-none d-sm-block"></div>
+
                     <div class="col-xs-12 col-sm-6">
                         {!! Form::open(['route' => 'apps.search', 'role' => 'form', 'method' => 'GET', 'class' => 'm-0']) !!}
                             <div class="searh-field tags-input__wrapper">
@@ -23,6 +25,7 @@
                             </div>
                         {!! Form::close() !!}
                     </div>
+
                     <div class="col-xs-12 col-sm-4 text-center">
                         <a href="{{ route('apps.paid') }}" class="btn btn-sm btn-white">{{ trans('modules.top_paid') }}</a>
                         <a href="{{ route('apps.new') }}" class="btn btn-sm btn-white">{{ trans('modules.new') }}</a>
