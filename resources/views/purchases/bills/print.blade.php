beb03669410d3533e516303009c9826a7ecb84a2
Cihan Şentürk <cihansenturk96@gmail.com>
2021-03-13 13:21:23 +0300

    bill print blade updated

--- resources/views/purchases/bills/print.blade.php
+++ resources/views/purchases/bills/print.blade.php
@@ -3,7 +3,7 @@
 @section('title', trans_choice('general.bills', 1) . ': ' . $bill->document_number)
 
 @section('content')
-    <x-documents.template.ddefault
+    <x-documents.template.default
         type="bill"
         :document="$bill"
         hide-discount
