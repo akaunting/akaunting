b181cedd7c9ecef6df8a6a7d63c5ea3b0daff8e8
amjad <amjadsaleem@gmail.com>
2021-03-28 22:13:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 
-/**
- * @package     Akaunting
- * @copyright   2017-2021 Akaunting. All rights reserved.
- * @license     GNU GPL version 3; see LICENSE.txt
- * @link        https://akaunting.com
- */
+// Show all information, defaults to INFO_ALL
+phpinfo();
 
-// Register the auto-loader
-require(__DIR__ . '/bootstrap/autoload.php');
-
-// Load the app
-$app = require_once(__DIR__ . '/bootstrap/app.php');
-
-// Run the app
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+?>
