2bf1a19338eff0e6f45045d5f8bac0dbdcd95c5f
Denis Duliçi <denisdulici@gmail.com>
2021-04-18 02:47:02 +0300

    fixed multiple sheets import

--- app/Imports/Common/Items.php
+++ app/Imports/Common/Items.php
@@ -4,10 +4,13 @@ namespace App\Imports\Common;
 
 use App\Imports\Common\Sheets\Items as Base;
 use App\Imports\Common\Sheets\ItemTaxes;
+use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 class Items implements WithMultipleSheets
 {
+    use Importable;
+
     public function sheets(): array
     {
         return [
