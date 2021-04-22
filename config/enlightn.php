d2154f8d6715b79b1d3517ae165d326e91b94a50
3li3bdalla <ali.abdalla@anbit.io>
2021-04-22 10:11:10 +0200

    add gpl-2.0 only to enlightn license_whitelist & change sync to database in .env.example queue connect

--- config/enlightn.php
+++ config/enlightn.php
@@ -129,7 +129,7 @@ return [
     'license_whitelist' => [
         'Apache-2.0', 'Apache2', 'BSD-2-Clause', 'BSD-3-Clause', 'LGPL-2.1-only', 'LGPL-2.1',
         'LGPL-2.1-or-later', 'LGPL-3.0', 'LGPL-3.0-only', 'LGPL-3.0-or-later', 'MIT', 'ISC',
-        'CC0-1.0', 'Unlicense', 'WTFPL', 'GPL-3.0-only', 'GPL-3.0-or-later', 'GPL-3.0+',
+        'CC0-1.0', 'Unlicense', 'WTFPL', 'GPL-3.0-only', 'GPL-3.0-or-later', 'GPL-3.0+',"GPL-2.0-only"
     ],
 
     /*
