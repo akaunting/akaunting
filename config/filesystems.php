c1f5d72d5782aabf3ce4416aa858d97c1eff1886
Denis Duliçi <denisdulici@gmail.com>
2021-05-12 13:26:12 +0300

    added remote media root

--- config/filesystems.php
+++ config/filesystems.php
@@ -81,6 +81,7 @@ return [
 
         's3' => [
             'driver' => 's3',
+            'root' =>  env('AWS_ROOT'),
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
