4f1185ed59f7640dba8c28ef71e2892489b570ba
Burak Civan <burakcivan22@gmail.com>
2021-06-21 16:34:59 +0300

    Ejected last commit

--- resources/views/banking/accounts/index.blade.php
+++ resources/views/banking/accounts/index.blade.php
@@ -30,11 +30,11 @@
                 <thead class="thead-light">
                     <tr class="row table-head-line">
                         <th class="col-sm-2 col-md-1 col-lg-1 col-xl-1 d-none d-sm-block">{{ Form::bulkActionAllGroup() }}</th>
-                        <th class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">@sortablelink('name', trans('general.name'), ['filter' => 'active, visible'], ['rel' => 'nofollow'])</th>
-                        <th class="col-md-2 col-lg-2 col-xl-2 d-none d-md-block text-left">@sortablelink('number', trans('accounts.number'))</th>
-                        <th class="col-sm-2 col-md-2 col-lg-2 col-xl-4 d-none d-sm-block text-right">@sortablelink('opening_balance', trans('accounts.current_balance'))</th>
+                        <th class="col-xs-4 col-sm-4 col-md-3 col-lg-3 col-xl-2">@sortablelink('name', trans('general.name'), ['filter' => 'active, visible'], ['class' => 'col-aka', 'rel' => 'nofollow'])</th>
+                        <th class="col-md-2 col-lg-2 col-xl-3 d-none d-md-block text-left">@sortablelink('number', trans('accounts.number'))</th>
+                        <th class="col-sm-2 col-md-2 col-lg-3 col-xl-4 d-none d-sm-block text-right">@sortablelink('opening_balance', trans('accounts.current_balance'))</th>
                         <th class="col-xs-4 col-sm-2 col-md-2 col-lg-2 col-xl-1">@sortablelink('enabled', trans('general.enabled'))</th>
-                        <th class="col-xs-4 col-sm-2 col-md-1 col-lg-1 col-xl-1 text-center">{{ trans('general.actions') }}</th>
+                        <th class="col-xs-4 col-sm-2 col-md-2 col-lg-1 col-xl-1 text-center">{{ trans('general.actions') }}</th>
                     </tr>
                 </thead>
 
@@ -44,10 +44,10 @@
                             <td class="col-sm-2 col-md-1 col-lg-1 col-xl-1 d-none d-sm-block">
                                 {{ Form::bulkActionGroup($item->id, $item->name) }}
                             </td>
-                            <td class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3 long-texts"><a href="{{ route('accounts.edit', $item->id) }}">{{ $item->name }}</a></td>
-                            <td class="col-md-2 col-lg-2 col-xl-2 d-none d-md-block text-left">{{ $item->number }}</td>
-                            <td class="col-sm-2 col-md-2 col-lg-2 col-xl-4 d-none d-sm-block text-right">@money($item->balance, $item->currency_code, true)</td>
-                            <td class="col-xs-4 col-sm-2 col-md-1 col-lg-2 col-xl-1">
+                            <td class="col-xs-4 col-sm-4 col-md-3 col-lg-3 col-xl-2"><a class="col-aka" href="{{ route('accounts.edit', $item->id) }}">{{ $item->name }}</a></td>
+                            <td class="col-md-2 col-lg-2 col-xl-3 d-none d-md-block text-left">{{ $item->number }}</td>
+                            <td class="col-sm-2 col-md-2 col-lg-3 col-xl-4 d-none d-sm-block text-right">@money($item->balance, $item->currency_code, true)</td>
+                            <td class="col-xs-4 col-sm-2 col-md-2 col-lg-2 col-xl-1">
                                 @if (user()->can('update-banking-accounts'))
                                     {{ Form::enabledGroup($item->id, $item->name, $item->enabled) }}
                                 @else
