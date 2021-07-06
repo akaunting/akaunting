01b619ce5bc662d6b92fee2eaad59c90b6c4de67
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-07-06 16:49:56 +0300

    remove unnecessary file #mvb7xp

--- app/Providers/ViewComposer.php
+++ app/Providers/ViewComposer.php
@@ -56,18 +56,6 @@ class ViewComposer extends Provider
             'partials.form.recurring', 'App\Http\ViewComposers\Recurring'
         );
 
-        // Add logo
-        View::composer(
-            ['purchases.bills.print', 'purchases.bills.show', 'sales.invoices.print_default', 'sales.invoices.print_classic', 'sales.invoices.print_modern', 'sales.invoices.show', 'portal.invoices.show', 'portal.invoices.signed'],
-            'App\Http\ViewComposers\Logo'
-        );
-
-        // Add Invoice Text
-        View::composer(
-            ['sales.invoices.*', 'portal.invoices.*'],
-            'App\Http\ViewComposers\InvoiceText'
-        );
-
         // Add Document Type
         View::composer(
             ['documents.*', 'portal.documents.*'],
