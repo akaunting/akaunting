935c53a5d9f6955acd42b1b30320d78ccf41ce0e
benguozakinci@gmail.com <akaunting-002@Akaunting-002s-MacBook-Pro.local>
2021-07-27 10:06:56 +0300

    Add prop for limiting character by default

--- resources/views/components/select-item-button.blade.php
+++ resources/views/components/select-item-button.blade.php
@@ -6,6 +6,7 @@
     price="{{ $price }}"
     :dynamic-currency="currency"
     :items="{{ json_encode($items) }}"
+    :search-char-limit="3"
     @item="onSelectedItem($event)"
     add-item-text="{{ trans('general.form.add_an', ['field' => trans_choice('general.items', 1)]) }}"
     create-new-item-text="{{ trans('general.title.create', ['type' =>  trans_choice('general.items', 1)]) }}"
