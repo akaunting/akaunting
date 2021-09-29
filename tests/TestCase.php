018eca597f543590706f6de5ccdf5dd4bf4a2bf5
Denis Duliçi <denisdulici@gmail.com>
2021-09-30 01:23:20 +0300

    user refresh db trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,20 +3,17 @@
 namespace Tests;
 
 use App\Traits\Jobs;
-use Illuminate\Support\Facades\Queue;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseMigrations, Jobs;
+    use CreatesApplication, Jobs, RefreshDatabase;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        Artisan::call('db:seed', ['--class' => '\Database\Seeds\TestCompany', '--force' => true]);
+        $this->artisan('db:seed', ['--class' => '\Database\Seeds\TestCompany', '--force' => true]);
     }
 }
