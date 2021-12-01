0d089703da49f06a8d30c4f7ccfd599a925aa55b
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-12-01 18:51:34 +0300

    close #2329 Fixed: Invoice/Bill edit missing notes and footer

--- app/Abstracts/View/Components/DocumentForm.php
+++ app/Abstracts/View/Components/DocumentForm.php
@@ -1039,6 +1039,10 @@ abstract class DocumentForm extends Base
             return $footerSetting;
         }
 
+        if (!empty($this->document)) {
+            return $this->document->footer;
+        }
+
         return setting($this->getSettingKey($this->type, 'footer'));
     }
 
@@ -1046,6 +1050,10 @@ abstract class DocumentForm extends Base
     {
         if (!empty($notesSetting)) {
             return $notesSetting;
+        }
+
+        if (!empty($this->document)) {
+            return $this->document->notes;
         }
 
         return setting($this->getSettingKey($this->type, 'notes'));
