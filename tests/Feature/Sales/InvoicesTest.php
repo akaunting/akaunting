87668494269127a2b388a6d1767efd5954e2ef00
Sevan Nerse <nerse.sevan@gmail.com>
2021-12-16 17:34:22 +0300

    test added #1xphe64

--- tests/Feature/Sales/InvoicesTest.php
+++ tests/Feature/Sales/InvoicesTest.php
@@ -123,6 +123,24 @@ class InvoicesTest extends FeatureTestCase
         ]);
     }
 
+    public function testItShouldNotCreateInvoiceWithNegativeQuantity()
+    {
+        $request = $this->getRequest();
+
+        $request['items'][0]['quantity'] = '-1';
+
+        $this->handleValidationExceptions();
+
+        $this->loginAs()
+            ->post(route('invoices.store'), $request)
+            ->assertRedirect()
+            ->assertInvalid(['items.0.quantity']);
+
+        $this->assertDatabaseMissing('documents', [
+            'document_number' => $request['document_number'],
+        ]);
+    }
+
     public function testItShouldSeeInvoiceUpdatePage()
     {
         $request = $this->getRequest();
