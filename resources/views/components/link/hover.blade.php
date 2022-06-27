57176801b3fe53331f48b0f9042819881d4e913a
Burak Civan <burakcivan22@gmail.com>
2022-06-27 15:10:32 +0300

    actions move to backend file

--- resources/views/components/link/hover.blade.php
+++ resources/views/components/link/hover.blade.php
@@ -1,15 +1,10 @@
-@props(['color', 'text', 'groupHover'])
-
-@php
-    if (empty($color)) {
-        $color = 'to-black';
-    }
-
-    if (empty($groupHover)) {
-        $groupHover = false;
-    }
-@endphp
-
-<span class="bg-no-repeat bg-0-2 bg-0-full {{ $groupHover ? 'group-hover:bg-full-2' : 'hover:bg-full-2' }} bg-gradient-to-b from-transparent {{ $color }} transition-backgroundSize">   
-  {!! $slot !!}
+<span
+    @class([
+        $color,
+        $groupHover ? 'group-hover:bg-full-2' : 'hover:bg-full-2',
+        'bg-no-repeat bg-0-2 bg-0-full bg-gradient-to-b from-transparent transition-backgroundSize'
+    ])
+>
+    {!! $slot !!}
 </span>
+
