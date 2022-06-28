4d48722d544307b69b49ab33268c7aa62844646f
Burak Civan <burakcivan22@gmail.com>
2022-06-28 15:32:56 +0300

    added form tag for form posting

--- resources/assets/js/views/install/Database.vue
+++ resources/assets/js/views/install/Database.vue
@@ -2,26 +2,16 @@
     <div>
         <InstallSteps :active_state="active"></InstallSteps>
 
-        <div class="card-body">
-                <div role="alert" class="alert alert-danger d-none" :class="(form.response.error) ? 'show' : ''" v-if="form.response.error" v-html="form.response.message"></div>
+        <div>
+            <div class="w-full bg-red-100 text-red-600 p-3 rounded-sm font-semibold text-xs" :class="(form.response.error) ? 'block' : 'hidden'" v-if="form.response.error" v-html="form.response.message"></div>
 
-                <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5 mt-0">
+            <form>
+                <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5">
                     <div class="sm:col-span-6 form-group required" :class="[{'has-error': form.errors.get('hostname')}]">
                         <label for="hostname" class="form-control-label">Hostname</label>
 
                         <div class="input-group input-group-merge">
-                            <input
-                                class="form-element"
-                                data-name="hostname"
-                                data-value="localhost"
-                                @keydown="form.errors.clear('hostname')"
-                                v-model="form.hostname"
-                                required="required"
-                                name="hostname"
-                                type="text"
-                                value="localhost"
-                                id="hostname"
-                            />
+                            <input class="form-element" data-name="hostname" data-value="localhost" @keydown="form.errors.clear('hostname')" v-model="form.hostname" required="required" name="hostname" type="text" value="localhost" id="hostname" />
                         </div>
 
                         <div class="text-red text-sm mt-1 block" v-if="form.errors.has('hostname')" v-html="form.errors.get('hostname')"></div>
@@ -57,17 +47,16 @@
                         <div class="text-red text-sm mt-1 block" v-if="form.errors.has('database')" v-html="form.errors.get('database')"></div>
                     </div>
                 </div>
-        </div>
 
-        <div class="relative__footer">
-            <div class="sm:col-span-6 flex items-center justify-end mt-3.5">
-                <button type="submit" @click="onSubmit" :disabled="form.loading" id="next-button" class="relative flex items-center justify-center bg-green hover:bg-green-700 text-white px-6 py-1.5 text-base rounded-lg disabled:bg-green-100">
-                    <i v-if="form.loading" class="animate-submit delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-3.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i>
-                    <span :class="[{'opacity-0': form.loading}]">
-                        Next
-                    </span>
-                </button>
-            </div>
+                <div class="sm:col-span-6 flex items-center justify-end mt-3.5">
+                    <button type="submit" @click="onSubmit" :disabled="form.loading" id="next-button" class="relative flex items-center justify-center bg-green hover:bg-green-700 text-white px-6 py-1.5 text-base rounded-lg disabled:bg-green-100">
+                        <i v-if="form.loading" class="animate-submit delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-3.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i>
+                        <span :class="[{'opacity-0': form.loading}]">
+                            Next
+                        </span>
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
--- resources/assets/js/views/install/Settings.vue
+++ resources/assets/js/views/install/Settings.vue
@@ -2,49 +2,49 @@
     <div>
         <InstallSteps :active_state="active"></InstallSteps>
 
-        <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5 mt-0">
-            <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('company_name')}]">
-                <label for="company_name" class="form-control-label">Company Name</label>
+        <form>
+            <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5 mt-0">
+                <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('company_name')}]">
+                    <label for="company_name" class="form-control-label">Company Name</label>
 
-                <div class="input-group input-group-merge">
-                    <input class="form-element" data-name="company_name" @keydown="form.errors.clear('company_name')" v-model="form.company_name" required="required" name="company_name" type="text" id="company_name" />
+                    <div class="input-group input-group-merge">
+                        <input class="form-element" data-name="company_name" @keydown="form.errors.clear('company_name')" v-model="form.company_name" required="required" name="company_name" type="text" id="company_name" />
+                    </div>
+
+                    <div class="text-red text-sm mt-1 block" v-if="form.errors.has('company_name')" v-html="form.errors.get('company_name')"></div>
                 </div>
 
-                <div class="text-red text-sm mt-1 block" v-if="form.errors.has('company_name')" v-html="form.errors.get('company_name')"></div>
-            </div>
+                <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('company_email')}]">
+                    <label for="company_email" class="form-control-label">Company Email</label>
 
-            <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('company_email')}]">
-                <label for="company_email" class="form-control-label">Company Email</label>
+                    <div class="input-group input-group-merge">
+                        <input class="form-element" data-name="company_email" @keydown="form.errors.clear('company_email')" v-model="form.company_email" required="required" name="company_email" type="text" id="company_email" />
+                    </div>
 
-                <div class="input-group input-group-merge">
-                    <input class="form-element" data-name="company_email" @keydown="form.errors.clear('company_email')" v-model="form.company_email" required="required" name="company_email" type="text" id="company_email" />
+                    <div class="text-red text-sm mt-1 block" v-if="form.errors.has('company_email')" v-html="form.errors.get('company_email')"></div>
                 </div>
 
-                <div class="text-red text-sm mt-1 block" v-if="form.errors.has('company_email')" v-html="form.errors.get('company_email')"></div>
-            </div>
+                <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('user_email')}]">
+                    <label for="user_email" class="form-control-label">Admin Email</label>
 
-            <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('user_email')}]">
-                <label for="user_email" class="form-control-label">Admin Email</label>
+                    <div class="input-group input-group-merge">
+                        <input class="form-element" data-name="user_email" @keydown="form.errors.clear('user_email')" v-model="form.user_email" required="required" name="user_email" type="text" id="user_email" />
+                    </div>
 
-                <div class="input-group input-group-merge">
-                    <input class="form-element" data-name="user_email" @keydown="form.errors.clear('user_email')" v-model="form.user_email" required="required" name="user_email" type="text" id="user_email" />
+                    <div class="text-red text-sm mt-1 block" v-if="form.errors.has('user_email')" v-html="form.errors.get('user_email')"></div>
                 </div>
 
-                <div class="text-red text-sm mt-1 block" v-if="form.errors.has('user_email')" v-html="form.errors.get('user_email')"></div>
-            </div>
+                <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('user_password')}]">
+                    <label for="user_password" class="form-control-label">Admin Password</label>
 
-            <div class="sm:col-span-6 required" :class="[{'has-error': form.errors.get('user_password')}]">
-                <label for="user_password" class="form-control-label">Admin Password</label>
+                    <div class="input-group input-group-merge">
+                        <input class="form-element" data-name="user_password" @keydown="form.errors.clear('user_password')" v-model="form.user_password" required="required" name="user_password" type="password" value="" id="user_password" />
+                    </div>
 
-                <div class="input-group input-group-merge">
-                    <input class="form-element" data-name="user_password" @keydown="form.errors.clear('user_password')" v-model="form.user_password" required="required" name="user_password" type="password" value="" id="user_password" />
+                    <div class="text-red text-sm mt-1 block" v-if="form.errors.has('user_password')" v-html="form.errors.get('user_password')"></div>
                 </div>
-
-                <div class="text-red text-sm mt-1 block" v-if="form.errors.has('user_password')" v-html="form.errors.get('user_password')"></div>
             </div>
-        </div>
 
-        <div class="relative__footer">
             <div class="sm:col-span-6 flex items-center justify-end mt-3.5">
                 <button type="submit" @click="onSubmit" :disabled="form.loading" id="next-button" class="relative flex items-center justify-center bg-green hover:bg-green-700 text-white px-6 py-1.5 text-base rounded-lg disabled:bg-green-100">
                     <i v-if="form.loading" class="animate-submit delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-3.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i>
@@ -53,7 +53,7 @@
                     </span>
                 </button>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
