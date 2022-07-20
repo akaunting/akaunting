70f53b635512f0cb1eedc3278460e68e6739ec7f
Cüneyt Şentürk <cneytsenturk@gmail.com>
2022-07-20 19:41:11 +0300

    remove user_role user_type column

--- database/migrations/2017_09_14_000000_core_v1.php
+++ database/migrations/2017_09_14_000000_core_v1.php
@@ -410,12 +410,11 @@ return new class extends Migration
         Schema::create('user_roles', function (Blueprint $table) {
             $table->integer('user_id')->unsigned();
             $table->integer('role_id')->unsigned();
-            $table->string('user_type');
 
             $table->foreign('role_id')->references('id')->on('roles')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            $table->primary(['user_id', 'role_id', 'user_type']);
+            $table->primary(['user_id', 'role_id']);
         });
 
         // Reconciliations
