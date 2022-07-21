c63f082ef226fcc840e104aee5625b10b72f205e
Denis Duliçi <denisdulici@gmail.com>
2022-07-22 00:30:51 +0300

    styling

--- resources/views/components/reports/summary/table/row.blade.php
+++ resources/views/components/reports/summary/table/row.blade.php
@@ -9,7 +9,7 @@
         <li
         @class([
             'mt-1' => $is_print,
-            'collapse-sub collapse-sub-report' => !$is_print
+            'collapse-sub collapse-sub-report' => ! $is_print
         ])
             data-collapse="child-{{ $parent_id }}">
         @else
@@ -18,7 +18,7 @@
             <div
                 @class([
                     'd-flex align-items-center justify-content-between' => $is_print,
-                    'flex justify-between border-0 m-0 p-0' => !$is_print
+                    'flex justify-between border-0 m-0 p-0' => ! $is_print
                 ])
             >
                 @if (isset($parent_id))
@@ -56,7 +56,7 @@
         <li
             @class([
                 'mt-1' => $is_print,
-                'collapse-sub collapse-sub-report' => !$is_print
+                'collapse-sub collapse-sub-report' => ! $is_print
             ])
             data-collapse="child-{{ $parent_id }}">
         @else
@@ -65,7 +65,7 @@
             <div style="display: flex; justify-content: space-between;">
                 <div style="display:flex; align-items: center; padding-left: {{ $tree_level * 20 }}px;">
                     <span>{{ $class->row_names[$table_key][$id] }}</span>
-                    @if (!$is_print)
+                    @if (! $is_print)
                         @if (array_sum($parent_row_values) != array_sum($class->row_values[$table_key][$id]))
                             <button type="button"  class="align-text-top flex" node="child-{{ $id }}" onClick="toggleSub('child-{{ $id }}', event)">
                                 <span class="material-icons transform rotate-90 transition-all text-lg leading-none">navigate_next</span>
@@ -84,7 +84,7 @@
     <li
         @class([
             'mt-1' => $is_print,
-            'collapse-sub collapse-sub-report' => !$is_print
+            'collapse-sub collapse-sub-report' => ! $is_print
         ])
         data-collapse="child-{{ $id }}">
         <div style="display: flex; justify-content: space-between;">
