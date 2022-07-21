ebfc67709a5102295392a8494441fb71f86dbf47
Denis Duliçi <denisdulici@gmail.com>
2022-07-21 16:36:34 +0300

    formatting

--- app/Widgets/ExpensesByCategory.php
+++ app/Widgets/ExpensesByCategory.php
@@ -22,7 +22,7 @@ class ExpensesByCategory extends Widget
                 $amount += $transaction->getAmountConvertedToDefault();
             });
 
-            $this->addMoneyToDonut($category->color, $amount, $category->name);
+            $this->addMoneyToDonut($category->colorHexCode, $amount, $category->name);
         });
 
         $chart = $this->getDonutChart(trans_choice('general.expenses', 2), '100%', 300, 6);
