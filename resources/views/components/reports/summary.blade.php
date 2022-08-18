096da300d9b40bed78bbb4cb04b359935927738f
Denis Duliçi <denisdulici@gmail.com>
2022-08-18 19:52:45 +0300

    styling

--- resources/views/components/reports/summary.blade.php
+++ resources/views/components/reports/summary.blade.php
@@ -5,8 +5,7 @@
 @include($class->views['summary.content.header'])
 
 @foreach($class->tables as $table_key => $table_name)
-    <div
-    class="flex flex-col lg:flex-row mt-12">
+    <div class="flex flex-col lg:flex-row mt-12">
         @include($class->views['summary.table'])
 
         @if (! $is_print)
