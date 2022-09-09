47090f03ded0bfd5edc3284df509b39bbf1e60d7
Burak Civan <burakcivan22@gmail.com>
2022-09-09 09:36:57 +0300

    rtl controlled

--- resources/views/components/documents/template/modern.blade.php
+++ resources/views/components/documents/template/modern.blade.php
@@ -206,12 +206,12 @@
         <div class="row">
             <div class="col-100">
                 <div class="text extra-spacing">
-                    <table class="lines modern-lines">
+                    <table class="lines modern-lines lines-radius-border">
                         <thead style="background-color:{{ $backgroundColor }} !important; -webkit-print-color-adjust: exact;">
                             <tr>
                                 @stack('name_th_start')
                                     @if (! $hideItems || (! $hideName && ! $hideDescription))
-                                        <td class="item text font-semibold text-alignment-left text-left text-white border-radius-first">
+                                        <td class="item text font-semibold text-alignment-left text-left text-white">
                                             {{ (trans_choice($textItems, 2) != $textItems) ? trans_choice($textItems, 2) : trans($textItems) }}
                                         </td>
                                     @endif
@@ -245,7 +245,7 @@
 
                                 @stack('total_th_start')
                                     @if (! $hideAmount)
-                                        <td class="total text font-semibold text-white text-alignment-right text-right border-radius-last">
+                                        <td class="total text font-semibold text-white text-alignment-right text-right">
                                             {{ trans($textAmount) }}
                                         </td>
                                     @endif
