b163b1a03b29fd3724fc8be25e2b4037dc08cb36
Burak Civan <burakcivan22@gmail.com>
2022-09-22 09:39:47 +0300

    loading component hidden feature

--- resources/views/components/loading/absolute.blade.php
+++ resources/views/components/loading/absolute.blade.php
@@ -1,4 +1,6 @@
-<div data-absolute-loading-content class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50">
-  <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
-</div>
+@if (! $hidden)
+  <div data-absolute-loading-content class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50">
+    <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
+  </div>
+@endif
 <!--data attr added because for none vue.js pages-->
