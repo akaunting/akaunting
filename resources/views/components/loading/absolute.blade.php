098d5a45b8db79c708318ed44d138030afeb1c4c
Burak Civan <burakcivan22@gmail.com>
2022-09-22 10:13:10 +0300

    unused codes ejected

--- resources/views/components/loading/absolute.blade.php
+++ resources/views/components/loading/absolute.blade.php
@@ -1,6 +1,4 @@
-@if (! $hidden)
-  <div data-absolute-loading-content class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50">
-    <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
-  </div>
-@endif
+<div data-absolute-loading-content class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50">
+  <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
+</div>
 <!--data attr added because for none vue.js pages-->
--- resources/views/components/loading/content.blade.php
+++ resources/views/components/loading/content.blade.php
@@ -1,6 +1,4 @@
-@if (! $hidden)
-  <div data-loading-content class="fixed w-full lg:w-4/5 h-screen flex items-center justify-center bg-body top-0 bottom-0 ltr:right-0 rtl:left-0 -mx-1 z-50">
-    <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28 lg:-mt-16" alt="Akaunting" />
-  </div>
-@endif
+<div data-loading-content class="fixed w-full lg:w-4/5 h-screen flex items-center justify-center bg-body top-0 bottom-0 ltr:right-0 rtl:left-0 -mx-1 z-50">
+  <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28 lg:-mt-16" alt="Akaunting" />
+</div>
 <!--data attr added because for none vue.js pages-->
