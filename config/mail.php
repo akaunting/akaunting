0204f0c0f644419b29860015356fe48faeb7264d
Denis Duliçi <denisdulici@gmail.com>
2022-09-26 12:22:49 +0300

    added symfony mailer packages

--- config/mail.php
+++ config/mail.php
@@ -54,6 +54,7 @@ return [
 
         'postmark' => [
             'transport' => 'postmark',
+            'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
         ],
 
         'sendgrid' => [
