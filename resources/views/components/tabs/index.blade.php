a8730e6ea30b08f32c14b7b09efb595eaf6c019a
Burak Civan <burakcivan22@gmail.com>
2022-09-28 10:14:43 +0300

    active tab control according to url

--- resources/views/components/tabs/index.blade.php
+++ resources/views/components/tabs/index.blade.php
@@ -1,6 +1,6 @@
 @props(['active'])
 
-<div x-data="{ active: '{{ $active }}' }">
+<div x-data="{ active: window.location.hash.split('#')[1] == undefined ? '{{ $active }}' : window.location.hash.split('#')[1] }">
     <div>
         <ul {{ ((! $attributes->has('override')) || ($attributes->has('override') && ! in_array('class', explode(',', $attributes->get('override'))))) ? $attributes->merge(['class' => 'flex items-center']) : $attributes }}>
             {!! $navs !!}
