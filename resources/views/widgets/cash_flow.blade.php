e522de171b5e5fbe5c702476c21ede0d8c1088f8
Burak Civan <burakcivan22@gmail.com>
2022-10-11 12:01:46 +0300

    cash flow responsive

--- resources/views/widgets/cash_flow.blade.php
+++ resources/views/widgets/cash_flow.blade.php
@@ -1,48 +1,48 @@
 <div id="widget-{{ $class->model->id }}" class="w-full my-8 px-12">
     @include($class->views['header'], ['header_class' => ''])
 
-    <div class="flex flex-col lg:flex-row mt-3">
-        <div class="w-full lg:w-11/12">
+    <div class="flex flex-col-reverse lg:flex-row mt-3">
+        <div class="w-full lg:w-11/12 apex-chart-cash-flow">
             {!! $chart->container() !!}
         </div>
 
-        <div class="w-full lg:w-1/12 mt-11 space-y-2">
-            <div class="flex flex-col items-center justify-between text-center">
+        <div class="w-96 lg:w-1/12 flex flex-row lg:flex-col items-center justify-around sm:justify-start lg:mt-11 space-y-0 sm:space-y-2">
+            <div class="relative flex flex-col items-center sm:justify-between text-center">
                 <div class="flex justify-end lg:block text-lg">
                     <x-tooltip id="tooltip-cashflow-incoming" placement="top" message="{{ $totals['incoming_exact'] }}">
                         {{ $totals['incoming_for_humans'] }}
                     </x-tooltip>
                 </div>
 
-                <span class="text-green text-xs">
+                <span class="text-green text-xs hidden lg:block">
                     {{ trans('general.incoming') }}
                 </span>
 
-                <span class="material-icons mt-2">remove</span>
+                <span class="absolute lg:relative material-icons mt-2 -right-12 lg:right-0">remove</span>
             </div>
 
-            <div class="flex flex-col items-center justify-between">
+            <div class="relative flex flex-col items-center sm:justify-between">
                 <div class="flex justify-end lg:block text-lg">
                     <x-tooltip id="tooltip-cashflow-outgoing" placement="top" message="{{ $totals['outgoing_exact'] }}">
                         {{ $totals['outgoing_for_humans'] }}
                     </x-tooltip>
                 </div>
 
-                <span class="text-rose text-xs">
+                <span class="text-rose text-xs hidden lg:block">
                     {{ trans('general.outgoing') }}
                 </span>
 
-                <span class="material-icons mt-2">drag_handle</span>
+                <span class="absolute lg:relative material-icons mt-2 -right-12 lg:right-0">drag_handle</span>
             </div>
 
-            <div class="flex flex-col items-center justify-between">
+            <div class="relative flex flex-col items-center sm:justify-between">
                 <div class="flex justify-end lg:block text-lg">
                     <x-tooltip id="tooltip-cashflow-profit" placement="top" message="{{ $totals['profit_exact'] }}">
                         {{ $totals['profit_for_humans'] }}
                     </x-tooltip>
                 </div>
 
-                <span class="text-purple text-xs">{{ trans_choice('general.profits', 1) }}</span>
+                <span class="text-purple text-xs hidden lg:block">{{ trans_choice('general.profits', 1) }}</span>
             </div>
         </div>
     </div>
