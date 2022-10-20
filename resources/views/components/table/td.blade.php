b7829cd19845f3fed2a5727b20a18989141d3e24
Burak Civan <burakcivan22@gmail.com>
2022-10-20 16:49:39 +0300

    added commands for text

--- resources/views/components/table/td.blade.php
+++ resources/views/components/table/td.blade.php
@@ -8,8 +8,11 @@
         }
     @endphp
     <div {{ $first_attributes }}>
+        <!-- first div for restrict of width. With javascript will add (overflow-x-hidden) class name -->
         <div>
+            <!-- There needs to be two div for disable/enable icons. If I don't create this div, animation will work with disable/enable icons -->
             <div>
+                <!-- this tag use for calculate width of text and parent element -->
                 <span data-truncate>
                     {!! $first !!}
                 </span>
