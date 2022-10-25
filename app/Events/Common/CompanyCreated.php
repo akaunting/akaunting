5eace1cbd0815e9f6366d94b23dca7cf2927c73c
Cüneyt Şentürk <cneytsenturk@gmail.com>
2022-10-25 10:15:07 +0300

    Company created event changes..

--- app/Events/Common/CompanyCreated.php
+++ app/Events/Common/CompanyCreated.php
@@ -8,13 +8,16 @@ class CompanyCreated extends Event
 {
     public $company;
 
+    public $request;
+
     /**
      * Create a new event instance.
      *
      * @param $company
      */
-    public function __construct($company)
+    public function __construct($company, $request = null)
     {
         $this->company = $company;
+        $this->request = $request;
     }
 }
