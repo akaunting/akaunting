176e5207e398d5033c740982b46d3ef41f98b75f
Burak Civan <burakcivan22@gmail.com>
2022-11-02 14:40:30 +0300

    update center feedbacks

--- resources/views/install/updates/index.blade.php
+++ resources/views/install/updates/index.blade.php
@@ -30,7 +30,7 @@
                             </x-table.td>
 
                             <x-table.td kind="right" class="w-6/12" kind="cursor-none">
-                                <x-slot name="first" class="text-right" override="class">
+                                <x-slot name="first" class="flex justify-end" override="class">
                                     <x-link href="{{ route('updates.run', ['alias' => 'core', 'version' => $core]) }}" class="px-3 py-1.5 rounded-xl text-sm font-medium leading-6 ltr:mr-2 rtl:ml-2 bg-green text-white hover:bg-green-700 disabled:bg-green-100" override="class">
                                         {{ trans('updates.update', ['version' => $core]) }}
                                     </x-link>
