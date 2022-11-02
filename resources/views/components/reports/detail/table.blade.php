2c74fd62122abad703217211de8712b91e69d0e3
Burak Civan <burakcivan22@gmail.com>
2022-11-02 12:24:54 +0300

    report table responsive control

--- resources/views/components/reports/detail/table.blade.php
+++ resources/views/components/reports/detail/table.blade.php
@@ -1,5 +1,5 @@
-<div class="overflow-x-visible mb-8 margin-bottom-spacing">
-    <table class="w-full rp-border-collapse">
+<div class="overflow-x-scroll large-overflow-unset mb-8 margin-bottom-spacing">
+    <table class="w-full small-table-width rp-border-collapse">
         @include($class->views['detail.table.header'])
         @include($class->views['detail.table.body'])
         @include($class->views['detail.table.footer'])
