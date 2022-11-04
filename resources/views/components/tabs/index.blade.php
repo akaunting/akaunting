b0c60c82e2ff7201a5ca802ae2b67ee8f7c0ffe3
Burak Civan <burakcivan22@gmail.com>
2022-11-04 14:23:53 +0300

    refactoring

--- resources/views/components/tabs/index.blade.php
+++ resources/views/components/tabs/index.blade.php
@@ -6,9 +6,15 @@
     } else {
         $slides = '3';
     }
+
+    if (! empty($attributes['is_slider'])) {
+        $is_slider = true;
+    } else {
+        $is_slider = false;
+    }
 @endphp
 
-<div data-swiper="{{ $slides }}" x-data="{ active: window.location.hash.split('#')[1] == undefined ? '{{ $active }}' : window.location.hash.split('#')[1] }">
+<div data-swiper="{{ $slides }}" data-is-slider="{{ $is_slider }}" x-data="{ active: window.location.hash.split('#')[1] == undefined ? '{{ $active }}' : window.location.hash.split('#')[1] }">
     <div data-tabs-swiper>
         <ul data-tabs-swiper-wrapper {{ ((! $attributes->has('override')) || ($attributes->has('override') && ! in_array('class', explode(',', $attributes->get('override'))))) ? $attributes->merge(['class' => 'flex items-center']) : $attributes }}>
             {!! $navs !!}
