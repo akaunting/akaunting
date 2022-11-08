a1d0ded44cb6b7ee9017eb04d6d2469a38a915d2
Burak Civan <burakcivan22@gmail.com>
2022-11-08 16:51:42 +0300

    code refactoring

--- resources/views/components/tabs/index.blade.php
+++ resources/views/components/tabs/index.blade.php
@@ -16,7 +16,7 @@
 
 <div data-swiper="{{ $slides }}" data-is-slider="{{ $is_slider }}" x-data="{ active: window.location.hash.split('#')[1] == undefined ? '{{ $active }}' : window.location.hash.split('#')[1] }">
     <div data-tabs-swiper>
-        <ul data-tabs-swiper-wrapper {{ ((! $attributes->has('override')) || ($attributes->has('override') && ! in_array('class', explode(',', $attributes->get('override'))))) ? $attributes->merge(['class' => 'flex items-center']) : $attributes }}>
+        <ul data-tabs-swiper-wrapper {{ ((! $attributes->has('override')) || ($attributes->has('override') && ! in_array('class', explode(',', $attributes->get('override'))))) ? $attributes->merge(['class' => 'inline-flex']) : $attributes }}>
             {!! $navs !!}
         </ul>
     </div>
