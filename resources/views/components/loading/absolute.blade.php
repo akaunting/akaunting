c973f806f1dec2d0a940b2df8da8229fd66cb502
Burak Civan <burakcivan22@gmail.com>
2022-11-21 15:48:48 +0300

    loading converted to alpine.js

--- resources/views/components/loading/absolute.blade.php
+++ resources/views/components/loading/absolute.blade.php
@@ -1,4 +1,9 @@
-<div data-absolute-loading-content class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50">
+<div
+  x-data="{ }"
+  x-init="document.querySelector('[data-modal-handle]') ? document.querySelector('[data-modal-handle]').classList.add('invisible'): null, setTimeout(() => $refs.loadingAbsoluteContent.remove(), 1000), setTimeout(() => document.querySelector('[data-modal-handle]') ? document.querySelector('[data-modal-handle]').classList.remove('invisible') : null , 1010)"
+  x-ref="loadingAbsoluteContent"
+  class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0 z-50"
+>
   <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
 </div>
 <!--data attr added because for none vue.js pages-->
