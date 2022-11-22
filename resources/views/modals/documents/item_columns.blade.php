bea2663fffb08593dc98471d51d31269a3cddc23
Burak Civan <burakcivan22@gmail.com>
2022-11-22 17:57:29 +0300

    js error fixed from edit column modal

--- resources/views/modals/documents/item_columns.blade.php
+++ resources/views/modals/documents/item_columns.blade.php
@@ -5,6 +5,7 @@
             label="{{ trans('settings.invoice.item_name') }}"
             :options="$item_names"
             :selected="$item_name"
+            change="settingsInvoice"
             input-name="item_name_input"
             :input-value="$item_name_input"
             form-group-class="sm:col-span-6 sm:gap-0"
@@ -15,6 +16,7 @@
             label="{{ trans('settings.invoice.price_name') }}"
             :options="$price_names"
             :selected="$price_name"
+            change="settingsInvoice"
             input-name="price_name_input"
             :input-value="$price_name_input"
             form-group-class="sm:col-span-6 sm:gap-0"
@@ -25,14 +27,13 @@
             label="{{ trans('settings.invoice.quantity_name') }}"
             :options="$quantity_names"
             :selected="$quantity_name"
+            change="settingsInvoice"
             input-name="quantity_name_input"
             :input-value="$quantity_name_input"
             form-group-class="sm:col-span-6 sm:gap-0"
         />
 
         <x-form.group.toggle name="hide_item_description" label="{{ trans('settings.invoice.hide.item_description') }}" :value="$hide_item_description" />
-
-        <x-form.group.toggle name="hide_quantity" label="{{ trans('settings.invoice.hide.quantity') }}" :value="$hide_quantity" />
         <x-form.group.toggle name="hide_amount" label="{{ trans('settings.invoice.hide.amount') }}" :value="$hide_amount" not-required form-group-class="sm:col-span-6" />
 
         <x-form.input.hidden name="type" :value="$type" />
