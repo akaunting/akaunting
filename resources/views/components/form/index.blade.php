5714269f4a9399f7a41ddfd945ac9b183bd10d40
Burak Civan <burakcivan22@gmail.com>
2022-11-28 17:58:41 +0300

    unused developments ejected

--- resources/views/components/form/index.blade.php
+++ resources/views/components/form/index.blade.php
@@ -17,7 +17,7 @@
     accept-charset="{{ $acceptCharset }}"
     @endif
     @submit.prevent="{{ $submit }}"
-    @keydown="form.errors ? form.errors.clear($event.target.name) : null"
+    @keydown="form.errors.clear($event.target.name)"
     {{ $attributes }}
 >
     @csrf
