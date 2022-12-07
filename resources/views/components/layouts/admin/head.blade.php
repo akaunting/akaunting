7d88423e24facfa6fc61b5a15d2b785545194b28
Burak Civan <burakcivan22@gmail.com>
2022-12-07 16:42:25 +0300

    styling loading converted to custom css

--- resources/views/components/layouts/admin/head.blade.php
+++ resources/views/components/layouts/admin/head.blade.php
@@ -5,6 +5,8 @@
 <head>
     @stack('head_start')
 
+    <link rel="stylesheet" href="{{ asset('public/css/custom_loading.css?v=' . version('short')) }}" type="text/css">
+    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- resources/views/components/loading/absolute.blade.php
+++ resources/views/components/loading/absolute.blade.php
@@ -2,9 +2,8 @@
   x-data="{ }"
   x-init="document.querySelector('[data-modal-handle]') ? document.querySelectorAll('[data-modal-handle]').forEach((item) => { item.classList.add('invisible') }) : null, setTimeout(() => $refs.loadingAbsoluteContent.remove(), 1000), setTimeout(() => document.querySelector('[data-modal-handle]') ? document.querySelectorAll('[data-modal-handle]').forEach((item) => { item.classList.remove('invisible') }) : null , 1010)"
   x-ref="loadingAbsoluteContent"
-  class="absolute w-full lg:flex items-start justify-center bg-body top-0 bottom-0 left-0 right-0"
-  style="z-index: 60;"
+  class="absolute-modal"
 >
-  <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28" alt="Akaunting" />
+  <img src="{{ asset('public/img/akaunting-loading.gif') }}" alt="Akaunting" />
 </div>
 <!--data attr added because for none vue.js pages-->
