9c1fcb430f136d8cdaf4b6b5da849f275298823d
Can Erdoğan <can@canerdogan.net>
2023-01-17 14:01:10 +0300

    token creator

---
+++ app/Console/Commands/CreateToken.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Auth\User;
+use Illuminate\Console\Command;
+
+class CreateToken extends Command
+{
+    public $user;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sanctum:token {user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate users token';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->user = User::find($this->argument('user'));
+
+        $token = $this->user->createToken('Api Token');
+
+        return ['token' => $token->plainTextToken];
+    }
+}
