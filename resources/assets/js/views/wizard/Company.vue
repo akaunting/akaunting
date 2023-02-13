6dc07b826fe82c0c4b52813ce53030a3de85c72d
Merve Karaman <amervekaraman@gmail.com>
2023-02-13 15:29:05 +0300

    Wizard overflow problem solved

--- resources/assets/js/views/wizard/Company.vue
+++ resources/assets/js/views/wizard/Company.vue
@@ -7,7 +7,7 @@
                 <span class="material-icons form-spin animate-spin text-9xl">data_usage</span>
             </div>
 
-            <div class="overflow-x-visible menu-scroll mt-1">
+            <div class="overflow-x-visible overflow-y-auto menu-scroll mt-1">
                 <form ref="form" class="py-2 align-middle inline-block min-w-full">
                     <div class="relative">
                         <div v-if="pageLoad" class="absolute left-0 right-0 top-0 bottom-0 w-full h-full bg-white rounded-lg flex items-center justify-center z-50">
