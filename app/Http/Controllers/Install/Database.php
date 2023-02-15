3553561240aa47cea92abc6727cfce30fbe14f67
EnesSacid-Buker <73346401+EnesSacid-Buker@users.noreply.github.com>
2023-02-15 17:04:59 +0300

    Code refactoring

--- app/Http/Controllers/Install/Database.php
+++ app/Http/Controllers/Install/Database.php
@@ -15,9 +15,12 @@ class Database extends Controller
      */
     public function create()
     {
-        $connection = config('database.default','mysql');
-
-        return view('install.database.create')->with(config("database.connections.$connection"));
+        return view('install.database.create', [
+            'host'      => env('DB_HOST'    , 'localhost'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'akaunting'),
+        ]);
     }
 
     /**
--- resources/views/install/database/create.blade.php
+++ resources/views/install/database/create.blade.php
@@ -5,7 +5,7 @@
 
     <x-slot name="content">
         <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5">
-            <x-form.group.text name="hostname" label="{{ trans('install.database.hostname') }}" value="{{ old('hostname', $host ?? 'localhost') }}" form-group-class="sm:col-span-6" />
+            <x-form.group.text name="hostname" label="{{ trans('install.database.hostname') }}" value="{{ old('hostname', $host) }}" form-group-class="sm:col-span-6" />
 
             <x-form.group.text name="username" label="{{ trans('install.database.username') }}" value="{{ old('username', $username) }}" form-group-class="sm:col-span-6" />
 
