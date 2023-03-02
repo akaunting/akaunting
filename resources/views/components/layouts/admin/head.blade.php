a31e13268cc4a5d4289d3a592c5410eb1fea634b
Cüneyt Şentürk <cneytsenturk@gmail.com>
2023-03-02 09:27:25 +0300

    typo

--- resources/views/components/layouts/admin/head.blade.php
+++ resources/views/components/layouts/admin/head.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     @stack('head_start')
-    
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,7 +15,7 @@
     <base href="{{ config('app.url') . '/' }}">
 
     <x-layouts.pwa.head />
-    
+
     <link rel="stylesheet" href="{{ asset('public/css/custom_loading.css?v=' . version('short')) }}" type="text/css">
 
     <!-- Favicon -->
