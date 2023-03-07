83dde159d09f39c8dff951f9765f6def24a5e2ac
PsyChaos <psychaos@gmail.com>
2023-03-07 18:02:43 +0300

    add sentry service

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -72,6 +72,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
         parent::report($exception);
     }
 
