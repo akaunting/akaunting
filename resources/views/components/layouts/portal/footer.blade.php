87d7d95e01219bbb31745a8db7ca62ec4a53b85c
Merve Karaman <amervekaraman@gmail.com>
2023-03-17 14:55:12 +0300

    Apps text in footer not appear

--- resources/views/components/layouts/portal/footer.blade.php
+++ resources/views/components/layouts/portal/footer.blade.php
@@ -1,5 +1,5 @@
 @stack('footer_start')
-    <footer class="footer">
+    <footer class="footer container">
         <div class="flex flex-col sm:flex-row items-center justify-between lg:mt-20 py-7 text-sm font-light">
             <div>
                 {{ trans('footer.powered') }}:
