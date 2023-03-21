ed9ce86adab846c87401af9652a82074fa79e947
EnesSacid-Buker <esbuker58@gmail.com>
2023-03-21 10:59:05 +0300

    Exists transaction number issue fixed

--- app/Traits/Transactions.php
+++ app/Traits/Transactions.php
@@ -234,7 +234,23 @@ trait Transactions
         $next   = setting('transaction' . $suffix . '.number_next');
         $digit  = setting('transaction' . $suffix . '.number_digit');
 
-        return $prefix . str_pad($next, $digit, '0', STR_PAD_LEFT);
+        $get_number = fn($prefix, $next, $digit) => $prefix . str_pad($next, $digit, '0', STR_PAD_LEFT);
+        $number_exists = fn($number) => Transaction::where('number', $number)->exists();
+
+        $transaction_number = $get_number($prefix, $next, $digit);
+
+        if ($number_exists($transaction_number)) {
+            do {
+                $next++;
+
+                $transaction_number = $get_number($prefix, $next, $digit);
+            } while ($number_exists($transaction_number));
+
+            setting(['transaction' . $suffix . '.number_next' => $next]);
+            setting()->save();
+        }
+
+        return $transaction_number;
     }
 
     public function increaseNextTransactionNumber($suffix = ''): void
