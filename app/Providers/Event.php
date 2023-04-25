2ce7e3e50a70af6de65b69dac8ebca78a24dc7ae
Cüneyt Şentürk <cneytsenturk@gmail.com>
2023-04-25 10:50:17 +0300

    Add new listener for 3.0.14

--- app/Providers/Event.php
+++ app/Providers/Event.php
@@ -22,6 +22,7 @@ class Event extends Provider
             'App\Listeners\Update\V30\Version307',
             'App\Listeners\Update\V30\Version309',
             'App\Listeners\Update\V30\Version3013',
+            'App\Listeners\Update\V30\Version3014',
         ],
         'Illuminate\Auth\Events\Login' => [
             'App\Listeners\Auth\Login',
