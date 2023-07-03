606c31a0d85911f4da37ac5b6fa0dd2c5a34f8b9
Merve Karaman <amervekaraman@gmail.com>
2023-07-03 12:08:10 +0300

    Wizard Finish stlye changed

--- resources/assets/js/views/wizard/Finish.vue
+++ resources/assets/js/views/wizard/Finish.vue
@@ -2,14 +2,14 @@
     <div class="relative bg-body z-10 rounded-lg shadow-2xl p-5 sm:p-10 full-height-mobile overflow-hidden">
         <WizardSteps :active_state="active"></WizardSteps>
 
-        <div class="flex flex-col justify-between -mt-5 sm:mt-0 overflow-y-scroll lg:overflow-hidden" style="height:523px;">
+        <div class="flex flex-col justify-between -mt-5 sm:mt-0" style="height:523px;">
             <div v-if="pageLoad" class="absolute left-0 right-0 top-0 bottom-0 w-full h-full bg-white rounded-lg flex items-center justify-center z-50">
                 <span class="material-icons form-spin animate-spin text-9xl">data_usage</span>
             </div>
 
             <div class="flex flex-col lg:flex-row mt-4">
                 <div class="w-full lg:w-1/2 ltr:pr-10 rtl:pl-10 mt-3">
-                    <div class="grid sm:grid-cols-6">
+                    <div class="grid sm:grid-cols-6 overflow-y-scroll lg:overflow-hidden scroll">
                         <h1 class="sm:col-span-6 text-black-300 mb-2 text-lg font-light">
                             {{ translations.finish.recommended_apps }}
                         </h1>
@@ -36,7 +36,7 @@
                     </div>
 
                     <div class="lg:hidden">
-                        <base-button class="btn flex items-center justify-center text-base disabled:opacity-50 relative mt-5 mx-auto bg-green hover:bg-gray-100 text-white rounded-md py-3 px-5 font-semibold" @click="finish()">
+                        <base-button class="btn flex items-center justify-center text-base disabled:opacity-50 relative mt-5 mx-auto bg-green hover:bg-gray-100 text-white rounded-md py-3 px-5 font-medium" @click="finish()">
                             {{ translations.finish.create_first_invoice }}
                         </base-button>
                     </div>
@@ -44,32 +44,36 @@
 
                 <div class="absolute w-1/2 right-0 ltr:pl-10 rtl:pr-10 mt-3 hidden lg:flex lg:flex-col">
                     <div class="flex">
-                        <img :src="image_src" class="absolute top-3 right-2 w-11/12 p-3 -mt-12" alt="Akaunting" />
+                        <img :src="image_src" class="absolute top-12 right-2 w-10/12 p-3 -mt-12" alt="Akaunting" />
                     </div>
 
-                    <base-button
-                        class="relative flex items-center justify-center text-base rounded-lg m-auto bottom-48 bg-white hover:bg-gray-100 text-purple py-3 px-5 font-semibold disabled:bg-gray-100 "
-                        :disabled="anchor_loading"
-                        @click="finish()"
-                    >
-                        <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
+                    <div class="flex">
+                        <base-button
+                            class="relative flex items-center justify-center text-base rounded-lg m-auto mt-96 hover:underline py-1.5 -right-20 font-semibold disabled:bg-gray-100 "
+                            :disabled="anchor_loading"
+                            @click="finish()"
+                            style="top: 5.9rem"
+                        >
+                            <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
 
-                        <span :class="[{'opacity-0': anchor_loading}]">
-                            {{ translations.finish.go_to_dashboard }}
-                        </span>
-                    </base-button>
+                            <span :class="[{'opacity-0': anchor_loading}]">
+                                {{ translations.company.skip }}
+                            </span>
+                        </base-button>
 
-                    <base-button
-                        class="relative flex items-center justify-center text-base rounded-lg m-auto top-10 mt-96 bg-white hover:bg-gray-100 text-purple py-3 px-5 font-semibold disabled:bg-gray-100 "
-                        :disabled="anchor_loading"
-                        @click="finish()"
-                    >
-                        <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
+                        <base-button
+                            class="relative flex items-center justify-center text-base rounded-lg m-auto mt-96 bg-default hover:bg-default-hover text-white py-1.5 px-7 font-medium"
+                            :disabled="anchor_loading"
+                            @click="finish()"
+                            style="top: 5.9rem; right: -0.6rem;"
+                        >
+                            <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
 
-                        <span :class="[{'opacity-0': anchor_loading}]">
-                            {{ translations.finish.skip }}
-                        </span>
-                    </base-button>
+                            <span :class="[{'opacity-0': anchor_loading}]">
+                                {{ translations.finish.create_first_invoice }}
+                            </span>
+                        </base-button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -149,6 +153,10 @@ export default {
     @media only screen and (max-width: 991px) {
         [modal-container] {
             height: 100% !important;
+        }
+
+        .scroll{
+            height:450px;
         }
     }
 </style>
