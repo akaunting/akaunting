7a589e5bbd7ae5922256c570162c1e394c85d5dc
Cüneyt Şentürk <cneytsenturk@gmail.com>
2023-07-07 11:49:31 +0300

    version update 3.0.15 to 3.0.16

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '0',
 
-    'patch'     =>  '15',
+    'patch'     =>  '16',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '31-May-2023',
+    'date'      =>  '7-July-2023',
 
-    'time'      =>  '17:00',
+    'time'      =>  '13:00',
 
     'zone'      =>  'GMT +3',
 
