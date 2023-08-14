e1fb015c67c95b97dd5c828a378f87e16f830473
Cihan Şentürk <53110792+CihanSenturk@users.noreply.github.com>
2023-08-14 14:04:04 +0300

    fixed import tax unique type issue

--- app/Imports/Settings/Taxes.php
+++ app/Imports/Settings/Taxes.php
@@ -12,6 +12,31 @@ class Taxes extends Import
 
     public function model(array $row)
     {
+        if (empty($row)) {
+            return;
+        }
+
         return new Model($row);
     }
+
+    public function map($row): array
+    {
+        $row = parent::map($row);
+        
+        if ($row['type'] == 'compound') {
+            $compound_tax = Model::where('type', 'compound')->first();
+
+            if ($compound_tax) {
+                $this->request_class = null;
+
+                // TODO: Add exception error
+                // throw new \Exception('Compound tax already exists.');
+
+                return [];
+            }
+        }
+
+        return $row;
+    }
+
 }
