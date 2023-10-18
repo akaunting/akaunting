b1c209a616e157c1f44d62e0c38e48567d90cfdb
Denis Duliçi <denisdulici@gmail.com>
2023-10-19 01:55:17 +0300

    changed module urls

--- app/Traits/Cloud.php
+++ app/Traits/Cloud.php
@@ -14,28 +14,50 @@ trait Cloud
             return route('roles.roles.index');
         }
 
-        if (request()->isNotCloudHost()) {
-            return 'https://akaunting.com/apps/roles?utm_source=software&utm_medium=' . $location . '&utm_campaign=roles';
+        if ($this->moduleExists('roles')) {
+            return route('apps.app.show', [
+                'alias'         => 'roles',
+                'utm_source'    => $location,
+                'utm_medium'    => 'app',
+                'utm_campaign'  => 'roles',
+            ]);
         }
 
-        return route('cloud.plans.index', [
-            'utm_source'    => $location,
-            'utm_medium'    => 'app',
-            'utm_campaign'  => 'roles',
-        ]);
+        if (request()->isCloudHost()) {
+            return route('cloud.plans.index', [
+                'utm_source'    => $location,
+                'utm_medium'    => 'app',
+                'utm_campaign'  => 'roles',
+            ]);
+        }
+
+        return 'https://akaunting.com/apps/roles?utm_source=software&utm_medium=' . $location . '&utm_campaign=roles';
     }
 
     public function getCloudBankFeedsUrl($location = 'widget')
     {
-        if (request()->isNotCloudHost()) {
-            return 'https://akaunting.com/apps/bank-feeds?utm_source=software&utm_medium=' . $location . '&utm_campaign=bank_feeds';
+        if ($this->moduleIsEnabled('bank-feeds')) {
+            return route('bank-feeds.bank-connections.index');
         }
 
-        return route('cloud.plans.index', [
-            'utm_source'    => $location,
-            'utm_medium'    => 'app',
-            'utm_campaign'  => 'bank_feeds',
-        ]);
+        if ($this->moduleExists('bank-feeds')) {
+            return route('apps.app.show', [
+                'alias'         => 'bank-feeds',
+                'utm_source'    => $location,
+                'utm_medium'    => 'app',
+                'utm_campaign'  => 'bank_feeds',
+            ]);
+        }
+
+        if (request()->isCloudHost()) {
+            return route('cloud.plans.index', [
+                'utm_source'    => $location,
+                'utm_medium'    => 'app',
+                'utm_campaign'  => 'bank_feeds',
+            ]);
+        }
+
+        return 'https://akaunting.com/apps/bank-feeds?utm_source=software&utm_medium=' . $location . '&utm_campaign=bank_feeds';
     }
 
     // @deprecated 3.1
