e64f6207773595048bb8f1aa9cf15de9d1441b96
Denis Duliçi <denisdulici@gmail.com>
2023-10-20 13:59:58 +0300

    formatting

--- app/Traits/Cloud.php
+++ app/Traits/Cloud.php
@@ -14,24 +14,12 @@ trait Cloud
             return route('roles.roles.index');
         }
 
-        if ($this->moduleExists('roles')) {
-            return route('apps.app.show', [
-                'alias'         => 'roles',
-                'utm_source'    => $location,
-                'utm_medium'    => 'app',
-                'utm_campaign'  => 'roles',
-            ]);
-        }
-
-        if (request()->isCloudHost()) {
-            return route('cloud.plans.index', [
-                'utm_source'    => $location,
-                'utm_medium'    => 'app',
-                'utm_campaign'  => 'roles',
-            ]);
-        }
-
-        return 'https://akaunting.com/apps/roles?utm_source=software&utm_medium=' . $location . '&utm_campaign=roles';
+        return route('apps.app.show', [
+            'alias'         => 'roles',
+            'utm_source'    => $location,
+            'utm_medium'    => 'app',
+            'utm_campaign'  => 'roles',
+        ]);
     }
 
     public function getCloudBankFeedsUrl($location = 'widget')
@@ -40,24 +28,12 @@ trait Cloud
             return route('bank-feeds.bank-connections.index');
         }
 
-        if ($this->moduleExists('bank-feeds')) {
-            return route('apps.app.show', [
-                'alias'         => 'bank-feeds',
-                'utm_source'    => $location,
-                'utm_medium'    => 'app',
-                'utm_campaign'  => 'bank_feeds',
-            ]);
-        }
-
-        if (request()->isCloudHost()) {
-            return route('cloud.plans.index', [
-                'utm_source'    => $location,
-                'utm_medium'    => 'app',
-                'utm_campaign'  => 'bank_feeds',
-            ]);
-        }
-
-        return 'https://akaunting.com/apps/bank-feeds?utm_source=software&utm_medium=' . $location . '&utm_campaign=bank_feeds';
+        return route('apps.app.show', [
+            'alias'         => 'bank-feeds',
+            'utm_source'    => $location,
+            'utm_medium'    => 'app',
+            'utm_campaign'  => 'bank_feeds',
+        ]);
     }
 
     // @deprecated 3.1
