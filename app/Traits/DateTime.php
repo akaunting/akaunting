c81ceaf71b2225c80ea1e61a1318ce81c6701843
Denis Duliçi <denisdulici@gmail.com>
2023-11-29 15:23:04 +0000

    fixed periodic dates

--- app/Traits/DateTime.php
+++ app/Traits/DateTime.php
@@ -260,6 +260,60 @@ trait DateTime
         return 'Y';
     }
 
+    public function getPeriodicDate(Date $date, string $period, string $year): string
+    {
+        switch ($period) {
+            case 'yearly':
+                $financial_year = $this->getFinancialYear($year);
+
+                if ($date->greaterThanOrEqualTo($financial_year->getStartDate()) && $date->lessThanOrEqualTo($financial_year->getEndDate())) {
+                    if (setting('localisation.financial_denote') == 'begins') {
+                        $formatted_date = $financial_year->copy()->getStartDate()->format($this->getYearlyDateFormat());
+                    } else {
+                        $formatted_date = $financial_year->copy()->getEndDate()->format($this->getYearlyDateFormat());
+                    }
+                }
+
+                break;
+            case 'quarterly':
+                $quarters = $this->getFinancialQuarters($year);
+
+                foreach ($quarters as $quarter) {
+                    if ($date->lessThan($quarter->getStartDate()) || $date->greaterThan($quarter->getEndDate())) {
+                        continue;
+                    }
+
+                    $start = $quarter->copy()->getStartDate()->format($this->getQuarterlyDateFormat($year));
+                    $end = $quarter->copy()->getEndDate()->format($this->getQuarterlyDateFormat($year));
+
+                    $formatted_date = $start . ' - ' . $end;
+                }
+
+                break;
+            case 'weekly':
+                $weeks = $this->getFinancialWeeks($year);
+
+                foreach ($weeks as $week) {
+                    if ($date->lessThan($week->getStartDate()) || $date->greaterThan($week->getEndDate())) {
+                        continue;
+                    }
+
+                    $start = $week->copy()->getStartDate()->format($this->getDailyDateFormat($year));
+                    $end = $week->copy()->getEndDate()->format($this->getDailyDateFormat($year));
+
+                    $formatted_date = $start . ' - ' . $end;
+                }
+
+                break;
+            default:
+                $formatted_date = $date->copy()->format($this->getMonthlyDateFormat($year));
+
+                break;
+        }
+
+        return $formatted_date;
+    }
+
     public function getTimezones(): array
     {
         // The list of available timezone groups to use.
