79bc046ee38bf3dc1e96fad78a043b93ed56de59
merve karaman <107546467+mervekaraman@users.noreply.github.com>
2024-01-05 16:36:05 +0300

    Widget alignment issue

--- resources/views/widgets/donut_chart.blade.php
+++ resources/views/widgets/donut_chart.blade.php
@@ -1,4 +1,4 @@
-<div id="widget-{{ $class->model->id }}" class="apexcharts-donut-custom {{ $class->model->settings->width }}">
+<div id="widget-{{ $class->model->id }}" class="apexcharts-donut-custom {{ $class->model->settings->width }} my-8">
     @include($class->views['header'], ['header_class' => ''])
 
     <div class="flex flex-col lg:flex-row mt-3" id="widget-donut-{{ $class->model->id }}">
