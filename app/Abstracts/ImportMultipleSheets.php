57c5a005a4029269885c357d3caf17de11d3c674
Cihan Şentürk <53110792+CihanSenturk@users.noreply.github.com>
2024-01-29 17:02:01 +0300

    Added import skip unknown sheets

--- app/Abstracts/ImportMultipleSheets.php
+++ app/Abstracts/ImportMultipleSheets.php
@@ -5,9 +5,10 @@ namespace App\Abstracts;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\SkipsUnknownSheets;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-abstract class ImportMultipleSheets implements ShouldQueue, WithChunkReading, WithMultipleSheets
+abstract class ImportMultipleSheets implements ShouldQueue, WithChunkReading, WithMultipleSheets, SkipsUnknownSheets
 {
     use Importable;
 
@@ -21,5 +22,10 @@ abstract class ImportMultipleSheets implements ShouldQueue, WithChunkReading, Wi
     public function chunkSize(): int
     {
         return config('excel.imports.chunk_size');
+    }
+    
+    public function onUnknownSheet($sheetName)
+    {
+        //
     }
 }
