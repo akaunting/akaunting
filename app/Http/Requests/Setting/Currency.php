4650fa12c9378530c7548cdbc79e9796fd27b12d
kreshnikg <kreshnikg3@gmail.com>
2024-04-04 13:20:36 +0200

    currency request

--- app/Http/Requests/Setting/Currency.php
+++ app/Http/Requests/Setting/Currency.php
@@ -25,7 +25,7 @@ class Currency extends FormRequest
 
         return [
             'name' => 'required|string',
-            'code' => 'required|string|unique:currencies,NULL,' . $id . ',id,company_id,' . $company_id . ',deleted_at,NULL',
+            'code' => 'required|string|unique:currencies,NULL,' . ($id ?? 'null') . ',id,company_id,' . $company_id . ',deleted_at,NULL',
             'rate' => 'required|gt:0',
             'enabled' => 'integer|boolean',
             'default_currency' => 'nullable|boolean',
