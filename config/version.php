5b988adb91de8feabcc092f3fe0dcd5546c96d74
Cihan Şentürk <cihansenturk96@gmail.com>
2025-02-14 15:52:30 +0300

    version update 3.1.14 to 3.1.15

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '1',
 
-    'patch'     =>  '14',
+    'patch'     =>  '15',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '23-December-2024',
+    'date'      =>  '14-February-2025',
 
-    'time'      =>  '20:00',
+    'time'      =>  '10:00',
 
     'zone'      =>  'GMT +3',
 
