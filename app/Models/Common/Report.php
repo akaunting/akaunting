bd0fe0e0befde13ed9de359f410055e7ffe27af4
Cihan Şentürk <53110792+CihanSenturk@users.noreply.github.com>
2025-04-29 20:50:09 +0300

    added new scopes to report model

--- app/Models/Common/Report.php
+++ app/Models/Common/Report.php
@@ -5,6 +5,7 @@ namespace App\Models\Common;
 use App\Abstracts\Model;
 use Bkwld\Cloner\Cloneable;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
 
 class Report extends Model
 {
@@ -41,6 +42,43 @@ class Report extends Model
         $class = ($alias == 'core') ? 'App\\\\' : 'Modules\\\\' . Str::studly($alias) . '\\\\';
 
         return $query->where('class', 'like', $class . '%');
+    }
+
+    /**
+     * Scope to only include reports of a given class.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $class
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeClass($query, $class)
+    {
+        return $query->where('class', '=', $class);
+    }
+
+    public function scopeExpenseSummary(Builder $query): Builder
+    {
+        return $query->where($this->qualifyColumn('class'), '=', 'App\\Reports\\ExpenseSummary');
+    }
+
+    public function scopeIncomeSummary(Builder $query): Builder
+    {
+        return $query->where($this->qualifyColumn('class'), '=', 'App\\Reports\\IncomeSummary');
+    }
+
+    public function scopeIncomeExpenseSummary(Builder $query): Builder
+    {
+        return $query->where($this->qualifyColumn('class'), '=', 'App\\Reports\\IncomeExpenseSummary');
+    }
+
+    public function scopeProfitLoss(Builder $query): Builder
+    {
+        return $query->where($this->qualifyColumn('class'), '=', 'App\\Reports\\ProfitLoss');
+    }
+
+    public function scopeTaxSummary(Builder $query): Builder
+    {
+        return $query->where($this->qualifyColumn('class'), '=', 'App\\Reports\\TaxSummary');
     }
 
     /**
