804cb8e5680f3649f996e089cf5848f999ec2493
Cihan Şentürk <cihansenturk96@gmail.com>
2026-03-09 22:44:15 +0300

    added dynamic validation for category code based on module status

--- app/Http/Requests/Setting/Category.php
+++ app/Http/Requests/Setting/Category.php
@@ -3,9 +3,11 @@
 namespace App\Http\Requests\Setting;
 
 use App\Abstracts\Http\FormRequest;
+use App\Traits\Modules;
 
 class Category extends FormRequest
 {
+    use Modules;
     /**
      * Get the validation rules that apply to the request.
      *
@@ -15,8 +17,11 @@ class Category extends FormRequest
     {
         $types = collect(config('type.category'))->keys();
 
+        $code = $this->moduleIsEnabled('double-entry') ? 'required|string' : 'nullable|string';
+
         return [
             'name' => 'required|string',
+            'code' => $code,
             'type' => 'required|string|in:' . $types->implode(','),
             'color' => 'required|string|colour',
         ];
