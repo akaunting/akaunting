eeae5567da6207967b8b4dfe97ff5b1edb1d43cf
Cüneyt Şentürk <cneytsenturk@gmail.com>
2026-03-14 13:08:31 +0000

    Fixed signed finished styling..

--- resources/views/portal/payments/signed.blade.php
+++ resources/views/portal/payments/signed.blade.php
@@ -17,7 +17,6 @@
             </x-link>
             @stack('button_print_end')
 
-
             @stack('button_dashboard_start')
             @if (! user())
                 <x-link href="{{ route('portal.dashboard') }}" class="px-3 py-1.5 mb-3 sm:mb-0 bg-gray-100 hover:bg-gray-200 rounded-lg text-sm font-medium leading-6">
