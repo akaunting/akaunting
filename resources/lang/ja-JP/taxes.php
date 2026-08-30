e7137fa89b9303d7312ce4555e24c2ef74f5cc52
BRICE ALI <bricealibyilingiro@gmail.com>
2026-08-30 09:56:02 +0200

    Update Japanese tax terms in translations

--- resources/lang/ja-JP/taxes.php
+++ resources/lang/ja-JP/taxes.php
@@ -2,10 +2,19 @@
 
 return [
 
-    'rate'                  => 'レート',
-    'rate_percent'          => '率 (%)',
+    'rate'                  => '税率',
+    'rate_percent'          => '税率（%）',
     'normal'                => '通常',
-    'inclusive'             => '包括的',
-    'compound'              => '化合物',
-    'fixed'                 => '修正済み',
+    'inclusive'             => '内税',
+    'compound'              => '複合税',
+    'fixed'                 => '固定額',
+    'withholding'           => '源泉徴収',
+    'no_taxes'              => '税金なし',
+    'create_task'           => '新しい税を作成し、設定からいつでも編集できます。',
+    'new_tax'               => '新しい税',
+    
+    'form_description' => [
+        'general'           => '内税は商品の価格に含めて計算されます。複合税は、他の税額を含めた金額に対して計算されます。固定税は割合ではなく、一定の金額として適用されます。',
+    ],
+
 ];
