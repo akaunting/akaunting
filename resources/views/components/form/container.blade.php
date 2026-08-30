ee141fb21c8286da60251ecfa6f24bdd9e131595
Cüneyt Şentürk <cneytsenturk@gmail.com>
2026-08-31 00:51:30 +0100

    Added form page loading feature..

--- resources/views/components/form/container.blade.php
+++ resources/views/components/form/container.blade.php
@@ -1,3 +1,5 @@
+<x-loading.content />
+
 <div class="flex flex-col lg:flex-row">
     <div {{ ((! $attributes->has('override')) || ($attributes->has('override') && ! in_array('class', explode(',', $attributes->get('override'))))) ? $attributes->merge(['class' => 'relative lg:w-8/12 z-10']) : $attributes }}>
         @if (! empty($head) && $head->isNotEmpty())
