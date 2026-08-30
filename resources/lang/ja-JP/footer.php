429a05e7f924859438f4ccbbb6ba0f03c5133d2c
BRICE ALI <bricealibyilingiro@gmail.com>
2026-08-30 09:50:06 +0200

    Modify footer translations in Japanese
    Updated software description and added new phrases for user guidance.

--- resources/lang/ja-JP/footer.php
+++ resources/lang/ja-JP/footer.php
@@ -5,6 +5,9 @@ return [
     'version'               => '製品版',
     'powered'               => 'Akauntingによって供給',
     'link'                  => 'https://akaunting.com',
-    'software'              => '無料会計ソフトウェア',
-
+    'software'              => 'オンライン会計ソフト',
+    'powered_by'            => '提供',
+    'tag_line'              => 'Akauntingで請求書を作成・送信し、経費を管理し、会計業務を自動化できます。:get_started_url',
+    'get_started'           => 'はじめる',
+    
 ];
